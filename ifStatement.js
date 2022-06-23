3358096cdbdcf4ea4cb6fea76181515c90abd8ae
mathinoj <88731748+mathinoj@users.noreply.github.com>
2022-06-23 14:36:20 +0300

    Add files via upload

---
+++ ifStatement.js
@@ -0,0 +1,9 @@
+console.log("herro");
+if(2 + 4 === 6){console.log('yea it dew')}
+let rando = Math.random();
+if(rando < 0.5){
+  console.log('Your number is less than .5')
+  console.log(rando);
+}
+
+// ELSE IF //
