b1db9f0cca2db18309918821c089c2b39fd438ce
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-06-25 15:01:58 +0300

    random number generator that applies discount

--- javascript/ifStatement.js
+++ javascript/ifStatement.js
@@ -1,9 +1,11 @@
 console.log("herro");
-if(2 + 4 === 6){console.log('yea it dew')}
+if (2 + 4 === 6) {
+    console.log("yea it dew");
+}
 let rando = Math.random();
-if(rando < 0.5){
-  console.log('Your number is less than .5')
-  console.log(rando);
+if (rando < 0.5) {
+    console.log("Your number is less than .5");
+    console.log(rando);
 }
 
 // ELSE IF //
@@ -11,35 +13,35 @@ if(rando < 0.5){
 let even = Math.floor(Math.random() * 100) + 1;
 // console.log("your num is " + " " + Math.floor(even));
 
-if(even % 2 === 0){
-  console.log("EVEN!! Your number is: " + even + "! This is an even number")
-    } else {
-    console.log("ODD number. Your number is: " + even + "! This is an odd number")
-    }
+if (even % 2 === 0) {
+    console.log("EVEN!! Your number is: " + even + "! This is an even number");
+} else {
+    console.log(
+        "ODD number. Your number is: " + even + "! This is an odd number"
+    );
+}
 
+// let even = (Math.random() * 100) + 1;
+// let rounded = Math.floor(even);
+// // console.log("your num is " + " " + Math.floor(even));
 
-    // let even = (Math.random() * 100) + 1;
-    // let rounded = Math.floor(even);
-    // // console.log("your num is " + " " + Math.floor(even));
+// if(rounded % 2 === 0){
+//   console.log("EVEN!! Your number is here: " + rounded + ". This is an even number")
+//     } else {
+//     console.log("ODD number. Your number is here: " + rounded + ". This is an odd number")
+//     }
 
-    // if(rounded % 2 === 0){
-    //   console.log("EVEN!! Your number is here: " + rounded + ". This is an even number")
-    //     } else {
-    //     console.log("ODD number. Your number is here: " + rounded + ". This is an odd number")
-    //     }
-
-
-    // ELSE IF ELSE
-    // const age = prompt('Enter age:');
-    // if (age <= 5){
-    //   console.log(age + " Free - baby");
-    // } else if (age <= 10) {
-    //   console.log(age + " $10 - child");
-    // } else if (age < 65){
-    //   console.log(age + " $15 - adult");
-    // } else {
-    //   console.log(age + " $10 - senior");
-    // }
+// ELSE IF ELSE
+// const age = prompt('Enter age:');
+// if (age <= 5){
+//   console.log(age + " Free - baby");
+// } else if (age <= 10) {
+//   console.log(age + " $10 - child");
+// } else if (age < 65){
+//   console.log(age + " $15 - adult");
+// } else {
+//   console.log(age + " $10 - senior");
+// }
 
 // let pword = prompt('Enter P-Word');
 // if(pword.length >= 6 && pword.indexOf(' ') === -1){
@@ -51,14 +53,14 @@ if(even % 2 === 0){
 // We did indexOf(' ') === -1 cuz having no spaces will give us a -1 index. Therefore when we run the indexOf(' ') it searches for a space in the pword. If user input a space then it will not equal to -1, cuz it would be true that they used a space. But if they does not input a space then it WILL equal to -1, which in this case is a good thing cuz we dont want pword to have spaces.
 
 const age = 10000;
-if((age >= 0 && age < 5) || age >= 65){
-  console.log('Free')
-} else if ( age >= 5 && age < 10){
-  console.log('$10')
-} else if (age >= 10 && age < 65){
-  console.log('$20')
-}else{
-  console.log('Wrong number foo!')
+if ((age >= 0 && age < 5) || age >= 65) {
+    console.log("Free");
+} else if (age >= 5 && age < 10) {
+    console.log("$10");
+} else if (age >= 10 && age < 65) {
+    console.log("$20");
+} else {
+    console.log("Wrong number foo!");
 }
 
 // function analyzeColor(color){
@@ -88,39 +90,105 @@ if((age >= 0 && age < 5) || age >= 65){
 //   alert(colorAsk + '? Imma haveta learn that one. ')
 // }
 
-
-var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 var randomColor = colors[Math.floor(Math.random() * colors.length)];
 
-function randy(){
-  if (colors === randomColor);
+function randy() {
+    if (colors === randomColor);
 }
 console.log(randomColor);
 
-    // let even = (Math.random() * 100) + 1;
+// let even = (Math.random() * 100) + 1;
 
-
-let luckyNum = (Math.floor(Math.random() * 6)) + 0;
-console.log(luckyNum)
+let luckyNum = Math.floor(Math.random() * 6) + 0;
+console.log(luckyNum);
 // function promo(){
-  if(luckyNum === 0){
-    console.log(luckyNum + ' is your number. No discount')
-  } else if (luckyNum === 1){
-    console.log(luckyNum + ' is your number. 10% discount')
-  } else if (luckyNum === 2){
-    console.log(luckyNum + ' is your number. 25% discount')
-  } else if (luckyNum === 3){
-    console.log(luckyNum + ' is your number. 35% discount')
-  } else if (luckyNum === 4){
-    console.log(luckyNum + ' is your number. 50% discount')
-  } else{
-    console.log(luckyNum + ' is your number. Errthing is FREE')
-  }
+if (luckyNum === 0) {
+    console.log(luckyNum + " is your number. No discount");
+} else if (luckyNum === 1) {
+    console.log(luckyNum + " is your number. 10% discount");
+} else if (luckyNum === 2) {
+    console.log(luckyNum + " is your number. 25% discount");
+} else if (luckyNum === 3) {
+    console.log(luckyNum + " is your number. 35% discount");
+} else if (luckyNum === 4) {
+    console.log(luckyNum + " is your number. 50% discount");
+} else {
+    console.log(luckyNum + " is your number. Errthing is FREE");
+}
 // }
 
+// function calculateTotal() {
+//     let grocery = prompt("How much grocery?");
+//     console.log(grocery);
+//     let numDisc = prompt("What's lucky num?");
+//     console.log(numDisc);
 
-let grocery = prompt('How much grocery?')
+//     if (numDisc === 0) {
+//         alert("Sorry no discount. Your total is " + grocery + numDisc);
+//     } else if (numDisc === 1) {
+//         alert("Your new total is " + (grocery - grocery * 0.1));
+//     }
+// }
+// calculateTotal();
 
-function calculateTotal (){
-  if (numDisc === 0)
+function calculateTool() {
+    let numbers = ["0", "1", "2", "3", "4", "5"];
+    let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    let totalCost = prompt("How much your grocery?");
+
+    if (randomNumber === "0") {
+        alert(
+            "Your number is: " +
+                randomNumber +
+                ". Sorry no discount. Your total is still: $" +
+                totalCost
+        );
+    } else if (randomNumber === "1") {
+        alert(
+            "Your number is: " +
+                randomNumber +
+                ", which gives you 10% discount. Your total is: $" +
+                (totalCost - 0.1 * totalCost) +
+                " ."
+        );
+    } else if (randomNumber === "2") {
+        alert(
+            "Your number is: " +
+                randomNumber +
+                ", which gives you 25% discount. Your total is: $" +
+                (totalCost - 0.25 * totalCost) +
+                " ."
+        );
+    } else if (randomNumber === "3") {
+        alert(
+            "Your number is: " +
+                randomNumber +
+                ", which gives you 35% discount. Your total is: $" +
+                (totalCost - 0.35 * totalCost) +
+                " ."
+        );
+    } else if (randomNumber === "4") {
+        alert(
+            "Your number is: " +
+                randomNumber +
+                ", which gives you 50% discount. Your total is: $" +
+                (totalCost - 0.5 * totalCost) +
+                " ."
+        );
+    } else {
+        randomNumber === "5";
+        alert(
+            "Your number is: " +
+                randomNumber +
+                ", which gives you 100% discount. Your total is: $" +
+                0 +
+                " ."
+        );
+    }
+
+    console.log(randomNumber);
+    console.log(totalCost);
 }
+calculateTool();
