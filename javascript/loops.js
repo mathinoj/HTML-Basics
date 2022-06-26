aee1dbb2bb2c3e8ec23056b08f39117923b8edda
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-06-26 14:42:48 +0300

    Exercise - loops. Uppercased names from array.

--- javascript/loops.js
+++ javascript/loops.js
@@ -1,3 +1,26 @@
-for (let x = 1; x <= 30; x += 2) {
-    console.log(x);
+// for (let x = 1; x <= 30; x += 2) {
+//     console.log(x);
+// }
+
+// for (let i = 100; i >= 0; i -= 2) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 100; i *= 2) {
+//     console.log(i);
+// }
+
+// for (let a = 1; a <= 10000; a *= 10) {
+//     console.log(a);
+// }
+
+// for (let i = 25; i >= 0; i -= 5) {
+//     console.log(i);
+// }
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
 }
