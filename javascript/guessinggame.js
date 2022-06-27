fe2b56f26895737f73484665c3e1681ca251b65d
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-06-27 16:45:46 +0300

    Finished guessing game. Also check your own notes

--- javascript/guessinggame.js
+++ javascript/guessinggame.js
@@ -1,18 +1,21 @@
 let enterMax = parseInt(prompt("Enter a maximum number."));
-while (!enterMax) {
-    enterMax = parseInt(prompt("Enter a maximum number."));
+console.log("1: " + enterMax);
+while (!enterMax || enterMax < 0) {
+    enterMax = parseInt(
+        prompt("That's not a valid number. Enter a maximum number.")
+    );
+    console.log("22: " + enterMax);
 }
+
 let userEnter = enterMax;
 console.log("userEnter = " + userEnter);
 let randomNum = Math.floor(Math.random() * userEnter) + 1;
 console.log("randomNum = " + randomNum);
-let guessNow = parseInt(
-    prompt(
+let guessNow = prompt(
+    userEnter +
+        " is the number you entered. Guess a number between 1 and " +
         userEnter +
-            " is the number you entered. Guess a number between 1 and " +
-            userEnter +
-            "."
-    )
+        "."
 );
 // let q = randomNum;
 let attempts = 1;
@@ -21,14 +24,20 @@ while (parseInt(guessNow) !== randomNum) {
     attempts++;
     if (guessNow > randomNum) {
         guessNow = prompt("Nums too high! Enter new guess.");
-    } else {
+    } else if (guessNow < randomNum) {
         guessNow = prompt("Nums too low. Guess again.");
+    } else {
+        guessNow = prompt(
+            "Your guess is: " +
+                guessNow +
+                ". This is an invalid value. Guess again!"
+        );
     }
 }
 if (guessNow === "q") {
     alert("You quitter.");
 } else {
-    alert("You guessed rite. It took you " + attempts + " attempt(s).");
+    alert("You guessed rite. Your number of attempts is: " + attempts + ".");
 }
 // } else {
 //   guessNow === "q";
