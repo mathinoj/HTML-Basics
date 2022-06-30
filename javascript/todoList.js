810cdb79b21576c7a2859c5c78200fffba461e84
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-06-30 09:53:51 +0300

    Working on delete. Have added create and view list

--- javascript/todoList.js
+++ javascript/todoList.js
@@ -17,23 +17,10 @@ let todoArray = ["sell", "trade"];
 let userInput = prompt("What would you LYK to do?").toLowerCase();
 // console.log(userInput);
 
-while (!commands.includes(userInput) || userInput !== "quit") {
-    userInput = prompt("I did not recognize that. What would you like to do?");
-    //     console.log(userInput);
-    // }
-
-    // console.log(todoArray);
-
-    // let userTodos = 1;
-
-    // while (commands.includes(userInput)) {
-    //     if (userInput === "quit") break;
-    // userTodos++;
-
+// while (!commands.includes(userInput) || userInput !== "quit") {
+while (userInput !== "quit") {
     if (userInput === "new") {
-        // let todoArray = [""];
-
-        let newTodo = prompt("You want to enter a new ToDo. Go ahead");
+        let newTodo = prompt("Enter a new ToDo.");
         console.log("LOOKY: " + newTodo);
         let addedTo = todoArray.push(newTodo);
         for (let i = 0; i < todoArray.length; i++);
@@ -42,32 +29,41 @@ while (!commands.includes(userInput) || userInput !== "quit") {
     }
 
     if (userInput === "list") {
-        let viewList = prompt(
-            "Here is the list in the Console. What else would you like to do?"
-        );
-
+        console.log("***************");
+        //  prompt("Here is the list in the Console. What else would you like to do?");
         for (let i = 0; i < todoArray.length; i++) {
             console.log(i + " " + todoArray[i]);
         }
+        console.log("***************");
+        userInput = alert("Here is the list in the Console.");
     } else if (userInput === "delete") {
-        let deleteTodo = prompt(
-            "You want to delete. Choose a number from the Console that you want to delete and input it below"
+        console.log("*******Delete********");
+        for (let i = 0; i < todoArray.length; i++) {
+            console.log(i + "-" + todoArray[i]);
+        }
+        console.log("*******Delete********");
+        let deleteTodo = parseInt(
+            prompt(
+                "You want to delete. Choose a number from the Console that you want to delete and input it below"
+            )
         );
         console.log(deleteTodo);
-        // } else if (userInput === "quit") {
-        //     let quitTodo = alert("Okay you quit");
-        //     console.log(quitTodo);
-    } else {
+        for (let i = 0; todoArray.length; i++) {
+            let;
+        }
+        //todoArray.pop(deleteTodo)
+    }
+    if (userInput === "quit") {
         userInput = prompt("What would you like to do THIS TIME?");
     }
 
-    // if (userInput === "quit") break;
-}
-
-if (userInput === "quit") {
-    let youSure = confirm("You sure you wanto to exit the ToDo App?");
-    // } else if (youSure === false) {
-    //     userInput = prompt("What would you like to do?").toLowerCase();
-} else {
-    userInput = prompt("What would you like to do IS THIS IT?").toLowerCase();
+    if (userInput === "quit") {
+        let youSure = confirm("You sure you wanto to exit the ToDo App?");
+        // } else if (youSure === false) {
+        //     userInput = prompt("What would you like to do?").toLowerCase();
+    } else {
+        userInput = prompt(
+            "What would you like to do IS THIS IT?"
+        ).toLowerCase();
+    }
 }
