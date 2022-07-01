440c60e49489a29827e4217e37437b32e34aa5b0
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-01 12:41:21 +0300

    Function that calculates sum of numbers in array

--- javascript/introFunctions.js
+++ javascript/introFunctions.js
@@ -42,9 +42,20 @@ function lastElement(arr) {
     return arr[arr.length - 1];
 }
 
+//Function that capitalizes 1st letter of string input
 function capitalize(strAgain) {
     let upper = strAgain[0].toUpperCase();
     let restOf = strAgain.slice(1, strAgain.length);
 
     return upper + restOf;
+}
+
+//Function that calculates all numbers in an array
+function sumArray(arrNums) {
+    let total = 0;
+    // let oneNum = arrNums.length;
+    for (let i = 0; i < arrNums.length; i++) {
+        total += arrNums[i];
+    }
+    return total;
 }
