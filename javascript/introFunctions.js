a5b36fb22461241b2aebdcad46cd9e37b90e2ac7
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-01 10:18:36 +0300

    Basic function properties modules/exercises

---
+++ javascript/introFunctions.js
@@ -0,0 +1,12 @@
+function name(matt) {
+    console.log("Yo " + matt);
+}
+
+function nameAgain(name) {
+    console.log("YoYo " + name);
+}
+function rant(message) {
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
