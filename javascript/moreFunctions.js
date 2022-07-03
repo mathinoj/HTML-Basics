c3f1e034d5dc2d92ba2b79271a2e97cb0332b7aa
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-03 10:32:29 +0300

    Completed exercise using 'this'

--- javascript/moreFunctions.js
+++ javascript/moreFunctions.js
@@ -8,3 +8,48 @@ function rollDye() {
     console.log(roll);
 }
 callTwo(rollDye);
+
+const cat = {
+    name: "Blu",
+    color: "grey",
+    breed: "cat",
+    mew: function () {
+        console.log("mew mew");
+    },
+};
+
+//SHORTHAND WOULD BE:
+const dog = {
+    name: "Black",
+    color: "tan",
+    breed: "dog",
+    woof() {
+        console.log("brk");
+    },
+};
+
+const human = {
+    name: "Matt",
+    color: "brown",
+    breed: "human",
+    say() {
+        console.log(this.name + " says yo!");
+    },
+};
+
+// const hen = {
+//     name: "Helen",
+//     eggCount: 0,
+//     layAnEgg: function () {
+//         return this.eggCount++;
+//     },
+// };
+
+const hen = {
+    name: "Helen",
+    eggCount: 0,
+    layAnEgg() {
+        this.eggCount++;
+        return "EGG";
+    },
+};
