35995591c259c10f6b68f70f03173274f6b9c3a5
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-05 12:57:37 +0300

    Added functional 'All' roast option.

--- javascript/coffeeProject.js
+++ javascript/coffeeProject.js
@@ -20,34 +20,37 @@ function renderCoffees(coffees) {
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value; //disregard
-    var filteredCoffees = []; //THIS var filterSrch
+    var selectedRoast = roastSelection.value;
+    console.log(selectedRoast);
+    // let allCoffee = allThem.value;
+    // console.log(allCoffee);
+    var filteredCoffees = [];
     coffees.forEach(function (coffee) {
-        //THIS change func Name
         if (coffee.roast === selectedRoast) {
-            //coffees[i] === lowCoff
-            filteredCoffees.push(coffee); // filterSrch.push(coffee)
+            filteredCoffees.push(coffee);
+        } else if (selectedRoast === "All") {
+            filteredCoffees.push(coffee);
         }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees); // renderCoffees(filtereSrch)
+    tbody.innerHTML = renderCoffees(filteredCoffees); //
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var rev = [
-    { id: 1, name: "Light City", roast: "light" },
-    { id: 2, name: "Half City", roast: "light" },
-    { id: 3, name: "Cinnamon", roast: "light" },
-    { id: 4, name: "City", roast: "medium" },
-    { id: 5, name: "American", roast: "medium" },
-    { id: 6, name: "Breakfast", roast: "medium" },
-    { id: 7, name: "High", roast: "dark" },
-    { id: 8, name: "Continental", roast: "dark" },
-    { id: 9, name: "New Orleans", roast: "dark" },
-    { id: 10, name: "European", roast: "dark" },
-    { id: 11, name: "Espresso", roast: "dark" },
-    { id: 12, name: "Viennese", roast: "dark" },
-    { id: 13, name: "Italian", roast: "dark" },
-    { id: 14, name: "French", roast: "dark" },
+    { id: 1, name: "Light City", roast: "Light" },
+    { id: 2, name: "Half City", roast: "Light" },
+    { id: 3, name: "Cinnamon", roast: "Light" },
+    { id: 4, name: "City", roast: "Medium" },
+    { id: 5, name: "American", roast: "Medium" },
+    { id: 6, name: "Breakfast", roast: "Medium" },
+    { id: 7, name: "High", roast: "Dark" },
+    { id: 8, name: "Continental", roast: "Dark" },
+    { id: 9, name: "New Orleans", roast: "Dark" },
+    { id: 10, name: "European", roast: "Dark" },
+    { id: 11, name: "Espresso", roast: "Dark" },
+    { id: 12, name: "Viennese", roast: "Dark" },
+    { id: 13, name: "Italian", roast: "Dark" },
+    { id: 14, name: "French", roast: "Dark" },
 ];
 
 var coffees = rev.reverse();
@@ -55,14 +58,17 @@ var coffees = rev.reverse();
 var tbody = document.querySelector("#coffees");
 var submitButton = document.querySelector("#submit");
 var roastSelection = document.querySelector("#roast-selection");
+let allThem = document.querySelector("#allCoffz");
 
 tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener("click", updateCoffees);
 
 //SEARCH FUNCTION********************************
+
 // var submitCoff = document.querySelector("#coffSearch");
 // submitCoff.addEventListener(onkeyup, updateCoffees);
+
 function userSearch() {
     let searchCoff = document.getElementById("userInput").value;
     console.log(searchCoff);
@@ -76,7 +82,7 @@ function userSearch() {
             lowCoff.includes(searchCoff)
         ) {
             filterSrch.push(coffees[i]); // filterSrch.push(coffee)
-            // console.log(coffees[i]);
+            console.log(coffees[i]);
         }
         tbody.innerHTML = renderCoffees(filterSrch);
     }
