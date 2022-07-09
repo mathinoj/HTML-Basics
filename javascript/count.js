594de38f5048b9c3d577a39f484df99d92f32efa
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-09 16:36:12 +0300

    Working on adding up function and event listener

---
+++ javascript/count.js
@@ -0,0 +1,15 @@
+function upOne(add) {
+    // let startNum = 0;
+    return (add += 1);
+}
+
+var upButton = document.querySelector("#upOne");
+upButton.addEventListener("click", upOne);
+// console.log(what);
+
+// var submitButton = document.querySelector("#submit");
+// var roastSelection = document.querySelector("#roast-selection");
+
+// tbody.innerHTML = renderCoffees(coffees);
+
+// submitButton.addEventListener("click", updateCoffees);
