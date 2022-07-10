aafda5cb1ee3ee78ea586d0d1408afcc73b1ae90
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-10 17:31:56 +0300

    Completed exercise 55, starting 56

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,7 +1,9 @@
-const div = document.querySelector("#container");
+// const div = document.querySelector("#container");
 
-div.style.textAlign = "center";
+// div.style.textAlign = "center";
 
-const image = document.querySelector("img");
-image.style.width = "150px";
-image.style.borderRadius = "50%";
+// const image = document.querySelector("img");
+// image.style.width = "150px";
+// image.style.borderRadius = "50%";
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
