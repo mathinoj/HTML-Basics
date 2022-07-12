c827ba7bf284d77cf233c29334a7ef3814b7391c
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-12 11:24:09 +0300

    Completed exercise 56 - rainbow/DOM

--- javascript/dom.js
+++ javascript/dom.js
@@ -7,3 +7,15 @@
 // image.style.borderRadius = "50%";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+// const allSpan = document.querySelectorAll("span");
+// let i = 0;
+// for (let spans of colors) {
+//     allSpan[i].style.color = spans;
+//     i++;
+// }
+
+const allSpan = document.querySelectorAll("span");
+for (let i = 0; i < colors.length; i++) {
+    allSpan[i].style.color = colors[i];
+}
