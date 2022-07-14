b813a7eaa1369a5ec6b073a1941cb62c40504c11
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-14 17:08:52 +0300

    My solution works on JS but not on Udemy

--- javascript/domTwo.js
+++ javascript/domTwo.js
@@ -54,26 +54,54 @@ function colorChange() {
 // });
 //******HAD TO BLOCK OUT LINE 2 FOR THIS TO WORK^^^^^^^^
 
+//WORKS BUT NOT, CORRECT ON UDEMY************************
+// const form = document.querySelector("form");
+// const listUL = document.querySelector("#list");
+
+// const inputProduct = document.querySelector("#product");
+// const inputQty = document.querySelector("#qty");
+// form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     const userInputProduct = form.elements.product;
+//     const userInputQty = form.elements.qty;
+//     addInputs(userInputProduct.value, userInputQty.value);
+//     userInputProduct.value = "";
+//     userInputQty.value = "";
+// });
+
+// const addInputs = (product, qty) => {
+//     const newProduct = document.createElement("li");
+//     newProduct.append(`${qty} `, product);
+//     list.appendChild(newProduct);
+// };
+
+//REFACTORED from solution above
 const form = document.querySelector("form");
+const listUL = document.querySelector("#list");
+
 const inputProduct = document.querySelector("#product");
 const inputQty = document.querySelector("#qty");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     const userInputProduct = form.elements.product;
     const userInputQty = form.elements.qty;
-    addInputs(userInputProduct.value, userInputQty.value);
+    const newProduct = document.createElement("li");
+    newProduct.innerText = `${qty.value} ${product.value}`;
+    listUL.appendChild(newProduct);
     userInputProduct.value = "";
     userInputQty.value = "";
 });
 
-const addInputs = (product, qty) => {
-    const newProduct = document.createElement("li");
-    // const newQty = document.createElement("li");
-    // newQty.append.product;
-    newProduct.append(`${qty} `, product);
-    // newProduct.appendChild.product;
-    // newQty.append("${qty}");
-    // newQty.appendChild.qty;
-
-    form.append(newProduct);
-};
+//REFACTORED Again!!!!!!!!
+// const form = document.querySelector("form");
+// const listUL = document.querySelector("#list");
+// form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     const inputProduct = document.querySelector("#product");
+//     const inputQty = document.querySelector("#qty");
+//     const newProduct = document.createElement("li");
+//     newProduct.innerText = `${qty.value} ${product.value}`;
+//     listUL.appendChild(newProduct);
+//     inputProduct.value = "";
+//     inputQty.value = "";
+// });
