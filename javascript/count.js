c80afef95c19d69f93b40303726a8ada941e826d
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-16 15:30:32 +0300

    Reset button brings all scores back to zero

--- javascript/count.js
+++ javascript/count.js
@@ -9,42 +9,37 @@ upButton.addEventListener("click", buttClick);
 let start = 0;
 
 function buttClick(e) {
-    //  e.stopPropagation();
-
     start++;
-    // console.log(e);
-    // let what = e.stopPropagation();
-    // console.log(`stopProp ${what}`);
 
     const scored = document.querySelector("#one");
     let showScore = (scored.innerText = start);
     console.log(showScore);
     e.stopPropagation();
-
-    let dButton = document.querySelector("#downOneClick");
-    dButton.addEventListener("click", buttDownClick);
-
-    let end = showScore;
-    function buttDownClick() {
-        end--;
-
-        const scoreDown = document.querySelectorAll("span");
-        let showScoreDown = (scoreDown.innerText = end);
-        console.log(showScoreDown);
-        // }
-        // console.log(upButton);
-
-        // let newStart = showScoreDown;
-        // console.log(`newStart ${newStart}`);
-        // function buttClick() {
-        //     // let start = 1;
-        //     // document.getElementById("upOneClick").innerText = start;
-        //     const scoredAgain = document.querySelector("h2");
-        //     let showScoreAgain = (scoredAgain.innerText = newStart);
-        //     console.log(showScoreAgain);
-    }
-    // return true;
 }
+
+let dButton = document.querySelector("#downOneClick");
+dButton.addEventListener("click", buttDownClick);
+
+let end = showScore;
+function buttDownClick() {
+    end--;
+
+    const scoreDown = document.querySelectorAll("span");
+    let showScoreDown = (scoreDown.innerText = end);
+    console.log(showScoreDown);
+    // }
+    // console.log(upButton);
+
+    // let newStart = showScoreDown;
+    // console.log(`newStart ${newStart}`);
+    // function buttClick() {
+    //     // let start = 1;
+    //     // document.getElementById("upOneClick").innerText = start;
+    //     const scoredAgain = document.querySelector("h2");
+    //     let showScoreAgain = (scoredAgain.innerText = newStart);
+    //     console.log(showScoreAgain);
+}
+// return true;
 
 let upButton2 = document.querySelector("#upOneClick2");
 upButton2.addEventListener("click", buttClick2);
@@ -68,7 +63,12 @@ function resetClicker() {
     const resetUno = document.querySelector("#one");
     const resetTwo = document.querySelector("#two");
     // let both = resetUno || resetTwo;
-
-    resetUno.innerHTML = resetStart;
-    resetTwo.innerHTML = resetStart;
+    start = 0;
+    startToo = 0;
+    resetUno.innerText = resetStart;
+    resetTwo.innerText = resetStart;
 }
+
+// resetAll.addEventListener("change", function () {
+//     resetingAll = parseInt(this.value);
+// });
