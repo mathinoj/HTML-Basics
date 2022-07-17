a60f7aa37ed467d3bb367676c0369a79893de400
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-17 17:05:41 +0300

    Have MVP for count project

--- javascript/count.js
+++ javascript/count.js
@@ -11,9 +11,10 @@ let start = 0;
 function buttClick(e) {
     start++;
 
-    let showScoreThis = document.getElementById("one").innerHTML;
-    console.log("NEED THIS one: " + showScoreThis);
+    // let showScoreThis = document.getElementById("one").innerHTML;
+    // console.log("NEED THIS one: " + showScoreThis);
     const scored = document.querySelector("#one");
+    console.log("NEED THIS one: " + start);
     scored.textContent = start;
 }
 
@@ -42,13 +43,30 @@ let upButton2 = document.querySelector("#upOneClick2");
 upButton2.addEventListener("click", buttClick2);
 let startToo = 0;
 function buttClick2(e) {
-    //  e.stopPropagation();
     startToo++;
-
-    const scored2 = document.querySelector("#two");
-    let showScore2 = (scored2.innerText = startToo);
-    console.log(showScore2);
+    // let scored2 = document.querySelector("#two").innerHTML;
+    const scoredAgain = document.querySelector("#two");
+    scoredAgain.innerText = startToo;
 }
+
+let dButton2 = document.querySelector("#downOneClick2");
+
+dButton2.addEventListener("click", function (e) {
+    let showScore2 = document.getElementById("two").innerHTML;
+
+    showScore2--;
+
+    const scoreDown2 = document.querySelector("#two");
+    let showScoreDown2 = (scoreDown2.innerText = showScore2);
+    console.log("You NEED this number " + showScoreDown2);
+
+    let upButtAgain2 = document.querySelector("#upOneClick2");
+    upButtAgain2.addEventListener("click", function (e) {
+        showScoreDown2++;
+        const scoring = document.querySelector("#two");
+        scoring.innerText = showScoreDown2;
+    });
+});
 
 var resetButt = document.querySelector("#resetClick");
 resetButt.addEventListener("click", resetClicker);
