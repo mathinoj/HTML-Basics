14dae97e677c6cb33053e8887a64a8b25754b20e
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-28 19:05:01 +0300

    Completed express routing basics

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -2,10 +2,35 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 
-app.use(() => {
-    console.log("We got new request");
-});
+// app.use((req, res) => {
+// console.log("We got new request");
+// console.dir(req);
+// res.send("We got your request. This is a response.");
+// res.send({ color: "red" });
+// res.send("<h1>My Webpage</h1>");
+// });
 //anytime we have an incoming request ^callback will run
+
+app.get("/", (req, res) => {
+    res.send("Homepage");
+});
+
+app.post("/cats", (req, res) => {
+    res.send("Post request to /cats. Diff from get request");
+});
+
+app.get("/cats", (req, res) => {
+    // console.log("Cat request");
+    res.send("Meow");
+});
+
+app.get("/dogs", (req, res) => {
+    res.send("Woof");
+});
+
+app.get("*", (req, res) => {
+    res.send("No conozco ese calle");
+});
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
