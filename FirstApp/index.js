bdffe774bc170073bc29f7795e6acd3add35d61e
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-28 18:03:52 +0300

    Followed his express demo

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 
+app.use(() => {
+    console.log("We got new request");
+});
+//anytime we have an incoming request ^callback will run
+
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
