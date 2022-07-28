712844a679badcaaba3849eddc5cf50215053ab3
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-28 15:23:04 +0300

    Finished requiring directory module

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,7 @@
-const math = require("./nodeMath");
+// const math = require("./nodeMath");
 // const {PI, square} = require("./nodeMath");
-console.log(math.PI);
-console.log(math.square(4));
+// console.log(math.PI);
+// console.log(math.square(4));
+
+const cats = require("./shelter");
+console.log("required a directory", cats);
