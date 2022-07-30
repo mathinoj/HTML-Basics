eb8c576a360e78531de5b1aea23614cb12daf2b5
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-30 17:33:22 +0300

    Express post routes demo

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.get("/tacos", (req, res) => {
+    res.send("Get /tacos response");
+});
+
+app.post("/tacos", (req, res) => {
+    res.send("POST /tacos response");
+});
+
 app.listen(3000, () => {
     console.log("On port 3000");
 });
