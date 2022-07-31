60262236011d481f8bc92b538d3cbbfee909ee7d
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-31 10:03:53 +0300

    Working on RESTful comments index module

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const comments = [
+    {
+        username: "Todd",
+        comment: "That funny",
+    },
+    {
+        username: "Rodd",
+        comment: "That funnier",
+    },
+    {
+        username: "Maude",
+        comment: "That funniest",
+    },
+    {
+        username: "Elrod",
+        comment: "That funniester",
+    },
+];
+//need to render these in a template so do next--->
+app.get("/comments", (req, res) => {
+    res.render("/comments/index"); //dont have to do index
+});
 
 app.get("/tacos", (req, res) => {
     res.send("Get /tacos response");
 });
 
 app.post("/tacos", (req, res) => {
-    res.send("POST /tacos response");
+    const { meat, qty } = req.body;
+    // console.log(req.body);
+    // res.send("POST /tacos response");
+    res.send(`Here your ${qty} ${meat} tacos`);
 });
 
 app.listen(3000, () => {
