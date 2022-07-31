9febb625f3f381022254990f36f7c2be6adc6c61
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-07-31 15:46:46 +0300

    Added update feature to edit posts

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 const { v4: uuid } = require("uuid");
 uuid();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -57,11 +59,21 @@ app.get("/comments/:id", (req, res) => {
     //Instead of 'show' it can be named anything
 });
 
-// app.patch("/comments/:id", (req, res) => {
-//     // res.send("Updating sumtin");
-//     const { id } = req.params;
-//     const comment = comments.find((c) => c.id === id);
-// });
+app.get("/comments/:id/edit", (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find((c) => c.id === id);
+    res.render("comments/edit", { comment });
+});
+
+app.patch("/comments/:id", (req, res) => {
+    // res.send("Updating sumtin");
+    const { id } = req.params;
+    // console.log(req.body.comment);
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find((c) => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect("/comments");
+});
 
 app.get("/tacos", (req, res) => {
     res.send("Get /tacos response");
