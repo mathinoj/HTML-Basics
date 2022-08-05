94acf35118df7ed5be7843d2574c423d960d73ae
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-05 16:49:05 +0300

    Mod 394 model instance method

--- MongooseBasics/products.js
+++ MongooseBasics/products.js
@@ -47,23 +47,53 @@ const productSchema = new mongoose.Schema({
 });
 //Doing it like this allows the REQUIRED validation
 
+// productSchema.methods.greet = function () {
+//     console.log("Hellllllloooooowwwww");
+//     console.log(`- FROM ${this.name}`);
+// };
+
+productSchema.methods.toggleOnSale = function () {
+    this.onsale = !this.onsale;
+    return this.save();
+    // this.save() refers to the particular instance of product
+};
+
+productSchema.methods.addCategory = function (newCat) {
+    this.categories.push(newCat);
+    return this.save();
+};
+
 const Product = mongoose.model("Product", productSchema);
 
-const bike = new Product({
-    name: "Cycling Hat",
-    price: 22,
-    categories: ["Mountain", "Trail"],
-    size: "XL",
-});
-bike.save()
-    .then((data) => {
-        console.log("It Worked");
-        console.log(data);
-    })
-    .catch((err) => {
-        console.log("Oh No Error");
-        console.log(err);
-    });
+const findProduct = async () => {
+    const foundProduct = await Product.findOne({ name: "Mountain Bike" });
+    // foundProduct.onSale = !foundProduct.onSale;
+    // foundProduct.save();
+    // ***********instead of doing the above two lines for an individual product you are extractign the logic and moving it onto the productSchema to an instance method so that there is access to it on every single instance of Product!
+    console.log(foundProduct);
+    await foundProduct.toggleOnSale();
+    console.log(foundProduct);
+    await foundProduct.addCategory("Outdoors");
+    console.log(foundProduct);
+};
+
+findProduct();
+
+// const bike = new Product({
+//     name: "Cycling Hat",
+//     price: 22,
+//     categories: ["Mountain", "Trail"],
+//     size: "XL",
+// });
+// bike.save()
+//     .then((data) => {
+//         console.log("It Worked");
+//         console.log(data);
+//     })
+//     .catch((err) => {
+//         console.log("Oh No Error");
+//         console.log(err);
+//     });
 
 // Product.findOneAndUpdate(
 //     { name: "Bike Pump" },
