4e9e28f3ee571fb7eb3bc926009a113b5f5a9573
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-05 15:26:28 +0300

    Created products.js for validation module

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -22,13 +22,13 @@ const Movie = mongoose.model("Movie", movieSchema);
 //In the above two lines we call an instance of a model. When doing this you NEED to call SAVE in order for it to take effect and save to the DB.
 
 //Using INSERTMANY directly inserts into our mongo DB
-Movie.insertMany([
-    { title: "Apollo 13", year: 1997, score: 9, rating: "PG-13" },
-    { title: "Pi", year: 1987, score: 4, rating: "R" },
-    { title: "Nope", year: 2022, score: 8, rating: "R" },
-    { title: "Dumbo", year: 1992, score: 2, rating: "G" },
-    { title: "Up", year: 2012, score: 7, rating: "G" },
-]).then((data) => {
-    console.log("It worked");
-    console.log(data);
-});
+// Movie.insertMany([
+//     { title: "Apollo 13", year: 1997, score: 9, rating: "PG-13" },
+//     { title: "Pi", year: 1987, score: 4, rating: "R" },
+//     { title: "Nope", year: 2022, score: 8, rating: "R" },
+//     { title: "Dumbo", year: 1992, score: 2, rating: "G" },
+//     { title: "Up", year: 2012, score: 7, rating: "G" },
+// ]).then((data) => {
+//     console.log("It worked");
+//     console.log(data);
+// });
