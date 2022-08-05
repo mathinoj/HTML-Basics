ca0007c4d897705e728b0493e5b474ef2d3454b7
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-05 16:13:16 +0300

    Mod 392 validating mongoose updates

--- MongooseBasics/products.js
+++ MongooseBasics/products.js
@@ -18,18 +18,53 @@ const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        // maxlength: 15,
     },
     price: {
         type: Number,
         required: true,
+        min: 0,
+    },
+    onSale: {
+        type: Boolean,
+        default: false,
+    },
+    categories: [String],
+    qty: {
+        online: {
+            type: Number,
+            default: 0,
+        },
+        inStore: {
+            type: Number,
+            default: 0,
+        },
     },
 });
 //Doing it like this allows the REQUIRED validation
 
 const Product = mongoose.model("Product", productSchema);
 
-const bike = new Product({ name: "Mountain Bike", price: 400 });
-bike.save()
+// const bike = new Product({
+//     name: "Bike Pump",
+//     price: 40,
+//     categories: ["Mountain", "Trail"],
+// });
+// bike.save()
+//     .then((data) => {
+//         console.log("It Worked");
+//         console.log(data);
+//     })
+//     .catch((err) => {
+//         console.log("Oh No Error");
+//         console.log(err);
+//     });
+
+Product.findOneAndUpdate(
+    { name: "Bike Pump" },
+    { price: -30.99 },
+    { new: true, runValidators: true }
+)
     .then((data) => {
         console.log("It Worked");
         console.log(data);
