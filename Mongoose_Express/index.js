6010dc0b30a2a25d04a7a0c386df08b14ecb4bd3
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-06 11:30:55 +0300

    Mod 400. Created seeds.js/hard coded collections

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+
+const Product = require("./models/product");
+
 mongoose
-    .connect("mongodb://localhost:27017/shoApp")
+    .connect("mongodb://localhost:27017/farmStand")
     .then(() => {
         console.log("MONGO connex open");
     })
--- Mongoose_Express/models/product.js
+++ Mongoose_Express/models/product.js
@@ -12,7 +12,13 @@ const productSchema = new mongoose.Schema({
     },
     category: {
         type: String,
+        lowercase: true,
         enum: ["fruit", "vegetable", "dairy"],
         // An enum is a special "class" that represents a group of constants (unchangeable variables, like final variables).
     },
 });
+
+const Product = mongoose.model("Product", productSchema);
+//This allows us to import the model (mongoose.model) and use it somewhere else
+
+module.exports = Product;
