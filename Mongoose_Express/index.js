3d2e25476f7ee3cb97fcc8e468f8030e5a78d35e
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-06 10:44:55 +0300

    Connected mongoose/express on index.js

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
-const path = required("path");
+const path = require("path");
+const mongoose = require("mongoose");
+mongoose
+    .connect("mongodb://localhost:27017/shoApp")
+    .then(() => {
+        console.log("MONGO connex open");
+    })
+    .catch((err) => {
+        console.log("ERROR with MONGO");
+        console.log(err);
+    });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.get("/dog", (req, res) => {
+    res.send("Bark");
+});
 
 app.listen(3000, () => {
     console.log("App be listening");
