a22fdba2d1f51415b837ed8aedf913e2ead2ed72
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-14 15:46:03 +0300

    444 added validated form class to form

--- Async_Errors/index.js
+++ Async_Errors/index.js
@@ -131,6 +131,19 @@ app.delete(
     })
 );
 
+const handleValidationErr = (err) => {
+    // console.log(err);
+    console.dir(err);
+
+    return new AppError(`Validation Failed...${err.message}`, 400);
+};
+
+app.use((err, req, res, next) => {
+    console.log(err.name);
+    if (err.name === "ValidationError") err = handleValidationErr(err);
+    next(err);
+});
+
 app.use((err, req, res, next) => {
     const { status = 500, message = "Something wrong" } = err;
     res.status(status).send(message);
