11cd895cc86b5b4fa37f352698f5d88330eab735
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-14 17:20:16 +0300

    407 continuation of errors

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,6 +3,7 @@ const path = require("path"); //2
 const mongoose = require("mongoose"); //3
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override"); //ADDDDDEEEEED
 const Campground = require("./models/campground"); //3
 
@@ -62,6 +63,8 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
     "/campgrounds",
     catchAsync(async (req, res, next) => {
+        if (!req.body.campground)
+            throw new ExpressError("Invalid camp data", 400);
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
@@ -105,10 +108,15 @@ app.delete(
         res.redirect("/campgrounds");
     })
 );
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page no found", 404));
+});
 //After making delete we need to make a button to send the delete the request. Its a form that will send a post request to the "/campgroundS/:id" URL but its going to fake out express and make it think its a delete request cuz we have input the methodOverride
 
 app.use((err, req, res, next) => {
-    res.send("Oh nah, suttin went wrong");
+    const { statusCode = 500, message = "Sumtin went Wrong" } = err;
+    res.status(statusCode).send(message);
 });
 
 //1
