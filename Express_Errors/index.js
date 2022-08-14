d7622042b7979cd63b029af7a650d399e3fb36da
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-14 11:10:58 +0300

    Mod 438 custom error handling class

--- Express_Errors/index.js
+++ Express_Errors/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+const AppError = require("./AppError");
+
 app.use(morgan("tiny"));
 app.use((req, res, next) => {
     req.requestTime = Date.now();
@@ -19,9 +21,10 @@ const verifyPassword = (req, res, next) => {
     if (password === "mattdogg") {
         next();
     }
+    throw new AppError("Password Required big doggy dogg", 401);
     // res.send("Need a p-word");
     // res.status(401);
-    throw new AppError(401, "Password Required big doggy dogg");
+    // throw new AppError(401, "Password Required big doggy dogg");
 };
 
 app.get("/error", (req, res) => {
@@ -42,18 +45,27 @@ app.get("/secret", verifyPassword, (req, res) => {
     res.send("What it do, SECRET boo.");
 });
 
+app.get("/admin", (req, res) => {
+    throw new AppError("You aint admin", 403);
+});
+
 app.use((req, res) => {
     // res.send("UNfounded it");
     res.status(404).send("UNfounded it");
 });
 
+// app.use((err, req, res, next) => {
+//     console.log("*&^*(^&^*&^*^^*^*^&(^*^%&");
+//     console.log("*&^*(^&^*&^*EEEEEERRRRRRRROOOOOOOOR^^*^*^&(^*^%&");
+//     console.log("*&^*(^&^*&^*^^*^*^&(^*^%&");
+// res.status(500).send("We got a problem my DOOD");
+// next(); if next() is empty it will call in the next middleware (or next non-error handling middleware, which is the verifyPassword const). When working with errors we'll want to pass the error inside next
+// next(err); this will call the next error handling middleware. If you dont call next(err) then you wont get the built-in error handler.
+// });
+
 app.use((err, req, res, next) => {
-    console.log("*&^*(^&^*&^*^^*^*^&(^*^%&");
-    console.log("*&^*(^&^*&^*EEEEEERRRRRRRROOOOOOOOR^^*^*^&(^*^%&");
-    console.log("*&^*(^&^*&^*^^*^*^&(^*^%&");
-    // res.status(500).send("We got a problem my DOOD");
-    // next(); if next() is empty it will call in the next middleware (or next non-error handling middleware, which is the verifyPassword const). When working with errors we'll want to pass the error inside next
-    next(err); //this will call the next error handling middleware. If you dont call next(err) then you wont get the built-in error handler.
+    const { status = 500, message = "Suttin went wrong" } = err;
+    res.status(status).send(message);
 });
 
 app.listen(3000, () => {
