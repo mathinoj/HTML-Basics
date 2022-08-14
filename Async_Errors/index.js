c334d3e878b2db416ac1bda3462d7439b5ed8d90
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-14 12:36:44 +0300

    441 made Asyn func to replace try/catches

--- Async_Errors/index.js
+++ Async_Errors/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const AppError = require("./AppError");
 
 const Product = require("./models/product");
 const { format } = require("path");
@@ -25,64 +26,114 @@ app.use(methodOverride("_method"));
 
 const categories = ["fruit", "vegetable", "dairy"];
 
-app.get("/products", async (req, res) => {
-    const { category } = req.query;
-    if (category) {
-        const products = await Product.find({ category });
-        res.render("products/index", { products, category });
-    } else {
-        const products = await Product.find({});
-        res.render("products/index", { products, category: "All" });
-    }
-    // const products = await Product.find({});
-    //^^this finds everything, matches every product
-    // This async route handler where we await some mongoose operation, we will do this all the time. So await Product.find({}) await Product.findByIdAndUpdate({}), await Product.remove({}).
-    // res.render("products/index", { products });
-});
+app.get(
+    "/products",
+    wrapAsync(async (req, res, next) => {
+        const { category } = req.query;
+        if (category) {
+            const products = await Product.find({ category });
+            res.render("products/index", { products, category });
+        } else {
+            const products = await Product.find({});
+            res.render("products/index", { products, category: "All" });
+        }
+        // const products = await Product.find({});
+        //^^this finds everything, matches every product
+        // This async route handler where we await some mongoose operation, we will do this all the time. So await Product.find({}) await Product.findByIdAndUpdate({}), await Product.remove({}).
+        // res.render("products/index", { products });
+    })
+);
 
 // we need an HTML form this should serve the form
 app.get("/products/new", (req, res) => {
+    // throw new AppError("UNallowed", 401); THIS IS USED AS A TEST
     res.render("products/new", { categories });
 });
 
 // app.post to /products should be where we submit the form and create a new product
 //when we have a post request and we want info from the post request body. we dont have access to request.body immediately. Instead we need to tell express to use app.use(express.urlencoded({extended: true})) (this is on line 21)
-app.post("/products", async (req, res) => {
-    const newProduct = new Product(req.body);
-    await newProduct.save();
-    // console.log(newProduct);
-    // res.send("makin product");
-    res.redirect(`/products/${newProduct._id}`);
-});
+app.post(
+    "/products",
+    wrapAsync(async (req, res, next) => {
+        const newProduct = new Product(req.body);
+        await newProduct.save();
+        // console.log(newProduct);
+        // res.send("makin product");
+        res.redirect(`/products/${newProduct._id}`);
+    })
+);
 
-app.get("/products/:id", async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    console.log(product);
-    res.render("products/show", { product });
-});
+// app.get("/products/:id", async (req, res, next) => {
+//     try {
+//         const { id } = req.params;
+//         const product = await Product.findById(id);
+//         if (!product) {
+//             throw new AppError("Product UNFound Brah", 404);
+//         }
+//         // console.log(product);
+//         res.render("products/show", { product });
+//     } catch (e) {
+//         next(e);
+//     }
+// });
 
-app.get("/products/:id/edit", async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.render("products/edit", { product, categories });
-});
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch((e) => next(e));
+    };
+}
 
-app.put("/products/:id", async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body, {
-        runValidators: true,
-        new: true,
-    });
-    res.redirect(`/products/${product._id}`);
-    // console.log(req.body)
-    // res.send("PUTT")
-});
+app.get(
+    "/products/:id",
+    wrapAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) {
+            throw new AppError("Product UNFound Brah", 404);
+        }
+        // console.log(product);
+        res.render("products/show", { product });
+    })
+);
 
-app.delete("/products/:id", async (req, res) => {
-    const { id } = req.params;
-    const deletedProduct = await Product.findByIdAndDelete(id);
-    res.redirect("/products");
+app.get(
+    "/products/:id/edit",
+    wrapAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) {
+            return next(new AppError("Product UNFound Broseph", 404));
+        }
+        res.render("products/edit", { product, categories });
+    })
+);
+
+app.put(
+    "/products/:id",
+    wrapAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body, {
+            runValidators: true,
+            new: true,
+        });
+        res.redirect(`/products/${product._id}`);
+        // console.log(req.body)
+        // res.send("PUTT")
+    })
+);
+
+app.delete(
+    "/products/:id",
+    wrapAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        res.redirect("/products");
+    })
+);
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Something wrong" } = err;
+    res.status(status).send(message);
 });
 
 app.listen(3000, () => {
