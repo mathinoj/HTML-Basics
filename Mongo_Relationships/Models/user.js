a47c4cfedcb1aabb10ff529ef8d5602d33ae9104
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-15 11:51:11 +0300

    454 one to few mod

--- Mongo_Relationships/Models/user.js
+++ Mongo_Relationships/Models/user.js
@@ -13,15 +13,44 @@ mongoose
 const userSchema = new mongoose.Schema({
     first: String,
     last: String,
-    address: [
+    addresses: [
         {
+            _id: { id: false }, //this turns off adding the ID, if we want this
             street: String,
             city: String,
             state: String,
-            country: {
-                type: String,
-                required: true,
-            },
+            country: String,
         },
     ],
 });
+
+const User = mongoose.model("User", userSchema);
+
+const makeUser = async () => {
+    const u = new User({
+        first: "Harry",
+        last: "Potter",
+    });
+    u.addresses.push({
+        street: "123 Sesame St.",
+        city: "New York",
+        state: "NY",
+        country: "USA",
+    });
+    const res = await u.save();
+    console.log(res);
+};
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push({
+        street: "12 Third St.",
+        city: "New York",
+        state: "NY",
+        country: "USA",
+    });
+    const res = await user.save();
+    console.log(res);
+};
+
+addAddress("62fa065bdabe998af57f7cfd");
