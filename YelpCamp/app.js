31769ea894ac4fc86a6eab028e73c4e8866bd3a0
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-17 16:58:09 +0300

    473 added full delete review functionality

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -152,6 +152,18 @@ app.post(
     })
 );
 
+app.delete(
+    "/campgrounds/:id/reviews/:reviewId",
+    catchAsync(async (req, res) => {
+        const { id, reviewId } = req.params;
+        await Campground.findByIdAndUpdate(id, {
+            $pull: { reviews: reviewId },
+        });
+        await Review.findByIdAndDelete(reviewId);
+        res.redirect(`/campgrounds/${id}`);
+    })
+);
+
 app.all("*", (req, res, next) => {
     next(new ExpressError("Page no found", 404));
 });
