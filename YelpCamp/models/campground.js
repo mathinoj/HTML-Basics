68b917df4ee3e5533fd51eb4198e06767f323750
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-18 15:54:38 +0300

    Created cookieDemo for mod 479

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema; //will reference this a lot when we get to relationships
 
 const CampgroundSchema = new Schema({
@@ -14,5 +15,21 @@ const CampgroundSchema = new Schema({
         },
     ],
 });
+
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+    // console.log("D FREAKIN LETED");
+    // console.log(doc);
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews,
+            },
+        });
+    }
+});
+
+//the id for each review is somewhere($in) our document.reviews(doc.reviews). The document is the array from the terminal/database. Were going to delete all reviews
+
+//this is a query middleware and its going to pass in a document if it found a document and deleted it to the function
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
