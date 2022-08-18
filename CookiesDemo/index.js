847a4c9d2f2465c3785aa4d99b1f7bebad73103c
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-18 16:37:56 +0300

    480 cookie parse middleware

--- CookiesDemo/index.js
+++ CookiesDemo/index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 
 const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 app.get("/greet", (req, res) => {
-    res.send("Yo there");
+    const { name = "Sin-Nombre" } = req.cookies;
+    res.send(`Hey there, ${name}`);
 });
 
 app.get("/setname", (req, res) => {
     res.cookie("name", "stevie O");
+    res.cookie("animal", "mini weenie");
     res.send("Sent cookie to cuz stevie");
 });
 
