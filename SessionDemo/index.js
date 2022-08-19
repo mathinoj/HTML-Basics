0333947db23370f7e78605e5ee67548d5ebb90cc
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-19 16:38:01 +0300

    485 express session intro

--- SessionDemo/index.js
+++ SessionDemo/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
+
+app.use(session({ secret: "thisisnotagoodsecret" }));
+
+app.get("/viewcount", (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`You have viewed this page this many times: ${req.session.count}`);
+});
 
 app.listen(3000, () => {
     console.log("Listen on 3000");
