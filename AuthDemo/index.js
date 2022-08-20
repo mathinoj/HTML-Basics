8dd337909aeab93f5db2d315d60e5dd410b2d6b6
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-20 22:26:07 +0300

    Added to index.js and created register.ejs

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const User = require("./models/user");
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
 app.get("/secret", (req, res) => {
     res.send("This is a secret. Cant be seen unless logged in");
 });
