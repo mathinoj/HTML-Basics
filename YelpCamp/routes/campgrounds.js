3d6e79bf0e7dd479078a692d30943ae1027a21ea
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-20 15:30:23 +0300

    Added validateCampground from app.js

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -4,6 +4,16 @@ const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
 const Campground = require("../models/campground");
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+};
+
 router.get(
     "/",
     catchAsync(async (req, res) => {
