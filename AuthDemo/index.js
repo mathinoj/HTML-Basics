75e793a576c4a5ee3c446e5eb4156ab55e0484ff
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-20 23:14:40 +0300

    503 authDemo registering

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -1,12 +1,43 @@
 const express = require("express");
+// const { default: mongoose } = require("mongoose");
 const app = express();
 const User = require("./models/user");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+mongoose
+    .connect("mongodb://localhost:27017/authDemo")
+    .then(() => {
+        console.log("MONGO connex open");
+    })
+    .catch((err) => {
+        console.log("ERROR with MONGO");
+        console.log(err);
+    });
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.urlencoded({ extended: true }));
+//req.body isnt being parsed so thats why we add this ^ so that we parse the req.body
+
+app.get("/", (req, res) => {
+    res.send("This be homepage");
+});
+
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash,
+    });
+    await user.save();
+    res.redirect("/");
 });
 
 app.get("/secret", (req, res) => {
