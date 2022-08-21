4400d8716a480271a04e8f8b1ec31728dab7991d
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-21 11:39:32 +0300

    Reformatted app.post('/login'...

--- AuthDemo/models/user.js
+++ AuthDemo/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -18,5 +19,6 @@ userSchema.statics.findAndValidate = async function (username, password) {
 };
 //using the ternary operator (?) return either the entire user that was found cuz we may want to use that for something. Cuz we need the ID, the bare minimum to put that in the session. Or if it was invalide we return false.
 //statics is where we can define multiple methods that will be added to the user class itself, to the model, not to the particular instance of user
+//doing this, we have grouped the logic that has to do with our user model on the user model, plus it shortens the code a lil bit
 
 module.exports = mongoose.model("User", userSchema);
