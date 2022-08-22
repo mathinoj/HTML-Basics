51fb5fff6f13c12553b424eff9f9c6fa12641214
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-22 21:59:40 +0300

    Added logout router to users.js

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -43,4 +43,14 @@ router.post(
     }
 );
 
+router.get("/logout", (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "Goodbye!");
+        res.redirect("/campgrounds");
+    });
+});
+
 module.exports = router;
