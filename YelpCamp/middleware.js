128a7902e8809e656fe73246a59cf03c307bd6ae
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-22 21:22:40 +0300

    added authentication function to middleware.js

---
+++ YelpCamp/middleware.js
@@ -0,0 +1,7 @@
+const isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.flash("error", "must first be signed in");
+        return res.redirect("/login");
+    }
+    next();
+};
