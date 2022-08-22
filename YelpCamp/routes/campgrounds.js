09a8babc7050ec99d3dfb9c89c455dbed88f94e9
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-22 21:21:34 +0300

    Created middleware.js

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -25,6 +25,10 @@ router.get(
 );
 
 router.get("/new", (req, res) => {
+    // if (!req.isAuthenticated()) {
+    //     req.flash("error", "must be signed in");
+    //     return res.redirect("/login");
+    // }
     res.render("campgrounds/new");
 });
 
