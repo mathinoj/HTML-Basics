bd6dbaf5295adbb0cec3a847bcdabd935da02100
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-23 19:33:39 +0300

    Newly registered user now doesn't have to re-login

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -10,14 +10,19 @@ router.get("/register", (req, res) => {
 
 router.post(
     "/register",
-    catchAsync(async (req, res) => {
+    catchAsync(async (req, res, next) => {
         try {
             const { email, username, password } = req.body;
             const user = new User({ email, username });
             const registeredUser = await User.register(user, password);
-            console.log(registeredUser);
-            req.flash("success", "Welcome to camp");
-            res.redirect("/campgrounds");
+            req.login(registeredUser, (err) => {
+                if (err) return next(err);
+                req.flash("success", "Welcome to camp");
+                res.redirect("/campgrounds");
+            });
+            // console.log(registeredUser);
+            // req.flash("success", "Welcome to camp");
+            // res.redirect("/campgrounds");
             //if all goes well user will get redirected to /campgrounds
         } catch (e) {
             req.flash("error", e.message);
