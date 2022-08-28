87bcf4190a0a6c9ec9381a627d2fb74541d4be50
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-08-28 16:02:13 +0300

    544 added thumbnail virtual property

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -2,14 +2,24 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema; //will reference this a lot when we get to relationships
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String,
+});
+//every image has a URL thats a string and a file name thats a string, and now images should be an array of images.
+
+ImageSchema.virtual("thumbnail").get(function () {
+    return this.url.replace("/upload", "/upload/w_200");
+});
+//this refers to the particular image. replace finds the first match with the set property of "/upload".
+
+//we use a virtual cuz we dont need to store it on the model or in the DB cuz its just derived from the information were already storing. were storing the URL. Were going to have to make a requrest to get the image anyway. It's not like were storing an image in MONGO, it's just the URL.
+
+//can only add virtual properties to a SCHEMA. But we want a virtual property on each image.
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String,
-        },
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
