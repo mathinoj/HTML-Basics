e00ebf0230facf5ec68db937a4b99c1698386639
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-11-06 16:14:11 +0300

    Added disable/enable functionality to up botton

--- javascript/belt.js
+++ javascript/belt.js
@@ -128,7 +128,7 @@ showForm.addEventListener("click", function (e) {
     // console.log(popo);
 
     const willForm = document.getElementById("beltSet").innerHTML;
-    console.log(willForm);
+    // console.log(willForm);
     const newBeltForm = document.createElement("form");
     console.log(newBeltForm);
     newBeltForm.setAttribute("id", `addedForm-${how}`);
@@ -139,7 +139,7 @@ showForm.addEventListener("click", function (e) {
     const getSubFormDiv = document.querySelector(`#addedForm-${how}`);
     console.log(getSubFormDiv);
     const sure = document.querySelector(".divFormSub").innerHTML;
-    console.log(sure);
+    // console.log(sure);
     const newby = document.createElement("div");
     console.log(newby);
     newby.setAttribute("id", `#divForSubmitForm-${how}`);
@@ -149,7 +149,7 @@ showForm.addEventListener("click", function (e) {
     const boo = document.getElementById(`#divForSubmitForm-${how}`);
     console.log(boo);
     const surely = document.querySelector(".buttSetBelt").innerHTML;
-    console.log(surely);
+    // console.log(surely);
     const booby = document.createElement("button");
     console.log(booby);
     booby.setAttribute("id", `#submitForm-${how}`);
@@ -169,48 +169,17 @@ showForm.addEventListener("click", function (e) {
     console.dir(e);
     // const formAdded = beltForming.append(newBeltForm);
     console.dir(e);
-    // console.dir(e.path[3].childNodes[5].childNodes);
-    // console.dir(e.path[3].childNodes[5].childNodes);
 
-    // console.dir(e.path[3].childNodes[5].childNodes[0].id);
-    // const replica = e.path[3].childNodes[5].childNodes[0];
     console.log(`addedForm-${how}`);
-
-    // const replica = e.path[3].childNodes[5].childNodes[0].id.replace(
-    //     "addedForm-",
-    //     ""
-    // );
-    // console.log(replica);
-    // console.log(formAdded);
 
     const yoloz = document.querySelector(`#addedForm-${how}`);
     const getDisplay = document.getElementById("getBelt").innerHTML;
-    console.log(getDisplay);
+    // console.log(getDisplay);
     const newDisplay = document.createElement("h3");
     console.log(newDisplay);
     newDisplay.setAttribute("id", `newDisplay-${how}`);
     newDisplay.innerHTML = getDisplay;
     yoloz.append(newDisplay);
-
-    // const meet = document.querySelector(`#addedForm-${how}`);
-    // console.log(meet);
-    // // let holla = `#addedForm-${how}`.replace("#addedForm-", "");
-    // // console.log(holla);
-    // const reed = document.querySelector(`#addedForm-${how}`)
-
-    // const getSubFormDiv = document.querySelector("#divForSubmitForm");
-    // const getSubFormDiv = document.querySelector(".divFormSub");
-
-    // holla++;
-    // console.log(grabNew);
-    // console.dir(grabNew[3]);
-    // const manny = document.querySelector("#submitForm");
-    // console.log(manny);
-
-    // const deduce = `counters-${how}`.replace("counters-", "");
-    // console.log(deduce);
-    // const classAdd = document.querySelector("#classCheck");
-    // classAdd.setAttribute("id", `#classCheck-${deduce}`);
 
     const recluse = `newDisplay-${how}`.replace("newDisplay-", "");
     console.log(recluse);
@@ -225,7 +194,7 @@ showForm.addEventListener("click", function (e) {
 
     // const yolo = document.querySelector(`#addedForm-${how}`);
     const getCounters = document.getElementById("displayBelt").innerHTML;
-    console.log(getCounters);
+    // console.log(getCounters);
     const newCountHeading = document.createElement("h4");
     console.log(newCountHeading);
     // newCountHeading.setAttribute("id", "counters");
@@ -248,23 +217,10 @@ showForm.addEventListener("click", function (e) {
     const newBeltChecker = document.querySelector("#newBelt");
     newBeltChecker.setAttribute("id", `#newBelt-${deduce}`);
 
-    // console.log(beltForming.append(newBeltForm));
-    // let numz = document.getElementById("formNum").innerText;
-    // console.log("Before numz++ (form): " + numz);
-    // numz++;
-    // console.log("AFTER numz++ (form): " + numz);
-    // const addFormer = document.querySelector("#formNum");
-    // const how = (addFormer.innerText = numz);
-    // // addFormer.innerText = "Form Number is: " + numz;
-    // // newCounterToForm.innerText = numz;
-    // console.log("Form number is: " + numz);
-
-    //GETS BUTTONS AND ADDS TO FORM
-    // const yolo = document.querySelector("#addedForm");
-    // const yolo = document.querySelector(`#addedForm-${how}`);
+    //GETS BUTTONS AND ADDS TO FORM*********************************
     console.log(yoloz);
     const getButts = document.getElementById("theButts").innerHTML;
-    console.log(getButts);
+    // console.log(getButts);
     const newButtDiv = document.createElement("div");
     console.log(newButtDiv);
     // newButtDiv.setAttribute("id", "countClass");
@@ -274,50 +230,13 @@ showForm.addEventListener("click", function (e) {
     // newButtsToForm.append(newButtDiv);
     // const buttonsAdded = newButtsToForm.append(newButtDiv);
     const buttonsAdded = yoloz.append(newButtDiv);
-    console.log(buttonsAdded);
+    // console.log(buttonsAdded);
     console.log(`countClass-${how}`);
     // console.log(countClass.length);
     console.dir(`countClass-${how}`);
-    // ^^^^ GETS BUTTONS AND ADDS TO FORM ^^^^^
-
-    // const getCounters = document.getElementById("displayBelt").innerHTML;
-    // console.log(getCounters);
-
-    // const newCountDiv = document.createElement("h4");
-    // console.log(newCountDiv);
-    // newCountDiv.setAttribute("id", "counters");
-    // newCountDiv.innerHTML = getCounters;
-    // yolo.append(newCountDiv);
-
-    //THIS SHOULD ADD A NUMBER (1, 2, 3, ...) TO EACH BUTTON
-    // let buttNumz = document.getElementById("buttNumber").innerText;
-    // console.log("BEFORE buttNumz++ : " + buttNumz);
-    // // let removeValQuotes = e.path[3].childNodes[9].attributes[1].value.replace(
-    // //     "",
-    // //     ""
-    // // );
-    // buttNumz++;
-    // console.log("AFTER buttNumz++ : " + buttNumz);
-    // const newlyVal = document.querySelector("#buttNumber");
-    // newlyVal.innerText = buttNumz;
-    // // newlyVal.innerText = "Butt Number is: " + buttNumz;
-    // console.log("Button number is: " + buttNumz);
-    //THIS SHOULD ADD A NUMBER (1, 2, 3, ...) TO EACH BUTTON ^^^^^^^
+    // ^^^^ GETS BUTTONS AND ADDS TO FORM ^^^^^^^^^^^^^^^^^^^^^^^^^
 
     console.dir(e.path);
-
-    //Class counter with up click COMMENTING OUT THIS PART MAKES 0 WORK PROPERLY
-    // console.log(newCounterToForm);
-    // const getClassCounter = document.getElementById("classCheck").innerHTML;
-    // console.log(getClassCounter);
-    // const newClassCounter = document.createElement("span");
-    // console.log(newClassCounter);
-    // newClassCounter.setAttribute("id", "classCheck");
-    // const goll = (newClassCounter.innerHTML = getClassCounter);
-    // console.log(goll);
-    // newCounterToForm.append(newClassCounter);
-    //Class counter with up click ^^^^^^^^
-    // COMMENTING OUT THIS PART MAKES 0 WORK PROPERLY^^^^^^^^^^^^^
 
     // const grabNew = document.querySelector("#addedForm");
     console.log(`countClass-${how}`);
@@ -346,27 +265,24 @@ showForm.addEventListener("click", function (e) {
     const stripeSpan = document.querySelector("#listStripes");
     console.dir(e);
     console.dir(e.path);
-    // console.dir(e.path[2]);
-    console.dir(e.path[3]);
-    // console.dir(e.path[2].childNodes);
-    console.dir(e.path[3].childNodes[5].innerText);
-    // console.dir(e.path[2].childNodes[11]);
-    // console.dir(e.path[2].childNodes[11].innerText);
+
+    // console.dir(e.path[3]);
+    // console.dir(e.path[3].childNodes[5].innerText);
+
     console.log(stripeSpan);
-    // const userInputClass = document.querySelector("#setClasses");
     const spanChangeClass = document.querySelector("#listClasses");
-    // const willis = document.querySelector("#listStripes");
+
     const grabClassCounter = document.querySelector("#classCheck");
     console.log(grabClassCounter);
     console.dir(e);
     const deez = document.querySelector(`#newDisplay-${how}`);
-    console.log(deez);
-    console.dir(deez);
-    console.dir(deez.childNodes[1]);
+    // console.log(deez);
+    // console.dir(deez);
+    // console.dir(deez.childNodes[1]);
     const deezerD = deez.childNodes[1];
-    console.log(deezerD);
-    console.dir(deez.childNodes[5]);
-    console.log(deez);
+    // console.log(deezerD);
+    // console.dir(deez.childNodes[5]);
+    // console.log(deez);
     const stripeToNeWForm = deez.childNodes[5];
     console.log(stripeToNeWForm);
     const classToNewForm = deez.childNodes[9];
@@ -392,31 +308,31 @@ showForm.addEventListener("click", function (e) {
         //THIS ADDS A COUNTER TO THE SUBMIT FORM ^^^^^^^^^^^^^^^^^^
 
         console.log(grabNew);
-        console.dir(grabNew);
+        // console.dir(grabNew);
 
         let bluey = (deezerD.innerHTML =
             "Belt Color: " + `${e.path[0][0].value}`);
         console.log(bluey);
         e.path[0][0].value = "";
 
-        console.log(spanChangeBelt);
+        // console.log(spanChangeBelt);
 
-        console.dir(e);
+        // console.dir(e);
 
-        console.log(stripeToNeWForm);
-        console.log(stripeToNeWForm.innerHTML);
-        console.dir(e);
-        console.dir(e.path[0][1].value);
+        // console.log(stripeToNeWForm);
+        // console.log(stripeToNeWForm.innerHTML);
+        // console.dir(e);
+        // console.dir(e.path[0][1].value);
 
         let striper =
             (stripeToNeWForm.innerText = `# of Stripes: ${e.path[0][1].value}`);
         e.path[0][1].value = "";
         console.log(striper);
 
-        console.log(stripeToNeWForm);
-        console.log(stripeToNeWForm.innerHTML);
-        console.dir(e);
-        console.dir(deez);
+        // console.log(stripeToNeWForm);
+        // console.log(stripeToNeWForm.innerHTML);
+        // console.dir(e);
+        // console.dir(deez);
 
         let classer =
             (classToNewForm.innerText = `# of Classes: ${e.path[0][2].value}`);
@@ -466,14 +382,6 @@ showForm.addEventListener("click", function (e) {
         console.dir(addClassez.childNodes[7]);
         // let addClass = addClassez.childNodes[7].innerHTML;
 
-        // let addClass = document.getElementById(
-        //     `#classCheck-${deduce}`
-        // ).innerText;
-        // console.log(addClass);
-        // console.dir(addClass);
-        // console.dir(grabNew);
-        // console.dir(grabNew.childNodes[26].childNodes[7]);
-        // console.log(grabNew);
         console.dir(e);
 
         console.dir(grabNew);
@@ -489,15 +397,15 @@ showForm.addEventListener("click", function (e) {
             e.preventDefault();
             console.dir(e);
             // console.log(stripeAdd);
-            console.log(`counters-${how}`);
-            console.log(setClass);
-            console.dir(setClass);
+            // console.log(`counters-${how}`);
+            // console.log(setClass);
+            // console.dir(setClass);
 
-            console.log(numz);
-            console.dir(e);
-            console.dir(numz);
+            // console.log(numz);
+            // console.dir(e);
+            // console.dir(numz);
 
-            console.log(grabNew);
+            // console.log(grabNew);
 
             console.dir(e);
 
@@ -559,37 +467,10 @@ showForm.addEventListener("click", function (e) {
             const getSetStripe = striper.replace("# of Stripes: ", "");
             console.log(getSetStripe);
 
-            // const deduce = `counters-${how}`.replace("counters-", "");
-            // console.log(deduce);
-            // const classAdd = document.querySelector("#classCheck");
-            // classAdd.setAttribute("id", `#classCheck-${deduce}`);
-
-            // const stipeChecker = document.querySelector("#stripeCheck");
-            // stipeChecker.setAttribute("id", `#stripeCheck-${deduce}`);
-
-            // const stripeAdd = document.querySelector("#stripeCheck");
-
             // console.log(stripeAdd);
-            console.dir(e);
-            console.dir(e.path[4].childNodes[9]);
-            console.dir(e.path[4].childNodes[9].childNodes);
-
-            // console.dir(stripeAdd);
-            // console.log(stripeAdd.childElementCount);
-
-            // let addClassez = document.getElementById(`counters-${how}`);
-            // console.log(addClassez);
-            // console.dir(addClassez.childNodes[7]);
-            // let addClass = addClassez.childNodes[7].innerHTML;
-
-            // const yoloz = document.querySelector(`#addedForm-${how}`);
-            // const getDisplay = document.getElementById("getBelt").innerHTML;
-            // console.log(getDisplay);
-            // const newDisplay = document.createElement("h3");
-            // console.log(newDisplay);
-            // newDisplay.setAttribute("id", `newDisplay-${how}`);
-            // newDisplay.innerHTML = getDisplay;
-            // yoloz.append(newDisplay);
+            // console.dir(e);
+            // console.dir(e.path[4].childNodes[9]);
+            // console.dir(e.path[4].childNodes[9].childNodes);
 
             let newLi = document.createElement("li");
             console.log(newLi);
@@ -605,9 +486,6 @@ showForm.addEventListener("click", function (e) {
             // alert(`up button clicked`);
 
             console.log(`countClass-${how}`);
-            // console.dir(countClass.childNodes[3]);
-            // const maybe = countClass.childNodes[3];
-            // console.log(maybe);
 
             for (let i = 1; i <= getSetStripe; i++) {
                 // for (let i = 1; i <= removedStripeText; i++) {
@@ -624,24 +502,19 @@ showForm.addEventListener("click", function (e) {
                 // console.log("This is grabClickUp: " + grabClickUp);
 
                 const timed = getSetClass * getSetStripe;
-                // const timed = removedClassText * removedStripeText;
-                // console.log("This is TIMED: " + timed);
-                // const lastNum = grabClickUp
-                const lastNum = classAdd.innerText;
-                // console.log("List-o-nums: " + lastNum);
-                // if (numToGet == grabClickUp) {
-                // console.log(stripeAdd);
 
-                console.dir(e.path);
-                console.dir(e.path[2].childNodes[26].childNodes[11]);
-                console.log(`counters-${how}`);
-                console.dir(`counters-${how}`);
-                console.dir(`newStripeSee-${how}`);
-                console.log(`newStripeSee-${how}`);
-                const playa = `counters-${how}`.replace("counters-", "");
-                console.log(playa);
-                const laya = `newStripeSee-${how}`.replace("newStripeSee-", "");
-                console.log(laya);
+                const lastNum = classAdd.innerText;
+
+                // console.dir(e.path);
+                // console.dir(e.path[2].childNodes[26].childNodes[11]);
+                // console.log(`counters-${how}`);
+                // console.dir(`counters-${how}`);
+                // console.dir(`newStripeSee-${how}`);
+                // console.log(`newStripeSee-${how}`);
+                // const playa = `counters-${how}`.replace("counters-", "");
+                // console.log(playa);
+                // const laya = `newStripeSee-${how}`.replace("newStripeSee-", "");
+                // console.log(laya);
                 console.log(addClassez);
 
                 if (numToGet == classAdd.innerText) {
@@ -659,15 +532,33 @@ showForm.addEventListener("click", function (e) {
                     addClassez.append(newBeltAdd);
 
                     alert("Advance to next belt!");
+
+                    let stopUp = document.getElementById(
+                        `#upOneClick-${reduce}`
+                    );
+                    stopUp.disabled = true;
+                    console.log(stopUp);
                     break;
                 }
             }
-            // alert("LOOP");
         });
 
         let addClassper = document.getElementById(`#downOneClick-${reduce}`);
         addClassper.disabled = true;
         console.log(addClassper);
+
+        const getSetClass = classer.replace("# of Classes: ", "");
+        console.log(getSetClass);
+
+        // const getSetStripe =
+        // const gettingStripe =
+        // document.querySelector("#listStripes").innerText;
+        // document.querySelector(`#listStripes-${recluse}`).innerText;
+        // console.log(gettingStripe);
+        console.log(striper);
+        // const getSetStripe = gettingStripe.replace("# of Stripes: ", "");
+        const getSetStripe = striper.replace("# of Stripes: ", "");
+        console.log(getSetStripe);
         minusClass.addEventListener("click", function (e) {
             e.preventDefault();
 
@@ -683,17 +574,19 @@ showForm.addEventListener("click", function (e) {
             );
             classSubtract.innerText = subtractClass;
 
+            const timed = getSetClass * getSetStripe;
+
             if (subtractClass <= 0) {
                 let addClassper = document.getElementById(
                     `#downOneClick-${reduce}`
                 );
                 addClassper.disabled = true;
                 console.log(addClassper);
-                // const getIt = document.getElementById(
-                //     `#classCheck-${deduce}`
-                // ).innerText;
-                // console.log(`#classCheck-${deduce}`);
-                // console.log(getIt);
+            } else if (subtractClass < timed) {
+                let baddy = document.getElementById(`#upOneClick-${reduce}`);
+                baddy.disabled = false;
+
+                console.log(baddy);
             }
         });
     });
