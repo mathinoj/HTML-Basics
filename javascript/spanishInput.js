86f5fee9605b89f3585e4c7c8a24113e17b51f6d
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-11-09 18:25:23 +0300

    created spanishInput html/js

---
+++ javascript/spanishInput.js
@@ -0,0 +1,12 @@
+let spanSubmit = document.getElementById(`#spanSubmit`);
+spanSubmit.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("click");
+});
+
+// let showForm = document.querySelector("#submitBelt");
+
+// showForm.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     console.log(beltForming);
+//     console.log(showForm);
