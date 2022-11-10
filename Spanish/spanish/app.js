794a6a431f1f6f0a3f67b2d0f4874995d56a1d2a
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-11-10 15:43:51 +0300

    Created basic express app for spanish project

---
+++ Spanish/spanish/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("viewengine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    res.send("home landing");
+});
+
+app.listen(3000, () => {
+    console.log("Connected port 3000");
+});
