c659e95fee4f3fb246d03c894be2277da2f983d4
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-11-10 16:10:23 +0300

    added mongoose to app.js

--- Spanish/app.js
+++ Spanish/app.js
@@ -1,13 +1,29 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const viewAll = require("./models/viewAll");
+
+mongoose.connect("mongodb://localhost:27017/Spanish", {});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connect error:"));
+db.once("open", () => {
+    console.log("Database Connected Mayngz");
+});
+
+const app = express();
 
 app.set("viewengine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
+app.get("/addSpanish", async (req, res) => {
     // res.send("home landing");
-    res.render("home");
+    // res.render("home");
+    const spanish = new spanishCard({
+        text: "How are you?",
+    });
+    await spanish.save();
+    res.send(spanish);
 });
 
 app.listen(3000, () => {
