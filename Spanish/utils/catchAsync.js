95c9a047fb6303187e1ff7c2c4935e8fb5136698
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-11-16 17:52:33 +0300

    Provided explanation of func in catchAsync.js

--- Spanish/utils/catchAsync.js
+++ Spanish/utils/catchAsync.js
@@ -3,3 +3,4 @@ module.exports = (func) => {
         func(req, res, next).catch(next);
     };
 };
+//weve created a function that returns and accepts a (req, res, next) function, then it executes the function 'func(req, res, next)' then catches any errors and passes it onto next()
