a049fb671cd2997770e5a9df81e02aefa316244a
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-11-19 16:50:36 +0300

    cleaned up card.js routers

--- Spanish/routes/card.js
+++ Spanish/routes/card.js
@@ -24,38 +24,16 @@ router.get(
 );
 
 router.get("/new", (req, res) => {
-    // const card = new Viewall({ english: "What", spanish: "Que" });
-    // await card.save();
     res.render("cards/new");
-    //BEFORE, THIS ROUTE WAS BELOW '/cards/:id/' but we moved it here because order matters
 });
 
 router.post(
     "/",
     validateCard,
     catchAsync(async (req, res, next) => {
-        // const SpanishSchema = Joi.object({
-        //     card: Joi.object({
-        //         card: Joi.string().required,
-        //         hint: Joi.string().required,
-        //         english: Joi.string().required,
-        //         spanish: Joi.string().required,
-        //         hintOne: Joi.string().required,
-        //         hintTwo: Joi.string().required,
-        //     }).required(),
-        // });
-        // const { error } = SpanishSchema.validate(req.body);
-        // if (error) {
-        //     const msg = error.details.map((el) => el.message).join(",");
-        //     throw new ExpressError(msg, 400);
-        // }
-        // if (!req.body.card) throw new ExpressError("Invalid card data!", 400);
         const newCard = new Viewall(req.body.card);
-        // const newCardHint = new Viewall(req.body.hint);
         await newCard.save();
-        // await newCardHint.save();
         res.redirect(`/cards/${newCard._id}`);
-        // res.send(req.body);
     })
 );
 
@@ -84,7 +62,6 @@ router.put(
             ...req.body.card,
         });
         res.redirect(`/cards/${editedCard._id}`);
-        // res.send("it TWERKED");
     })
 );
 
