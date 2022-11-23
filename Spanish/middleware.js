66f49a057d38f46fb103db4995f8804ba511a01d
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-11-23 21:52:03 +0300

    Added three consts to middleware.js

--- Spanish/middleware.js
+++ Spanish/middleware.js
@@ -1,3 +1,7 @@
+const { spanishSchemaAlso, reviewSchema } = require("./schemas.js");
+const ExpressError = require("./utils/ExpressError");
+const Travelall = require("./models/viewAllTravel");
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.flash("error", "Must be signed in first, mayngs!");
