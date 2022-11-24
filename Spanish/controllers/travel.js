a5b33eb50708e68cd66843b027d3178a321c5f98
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-11-24 15:01:42 +0300

    Updated travel contoller routes

--- Spanish/controllers/travel.js
+++ Spanish/controllers/travel.js
@@ -1,57 +1,118 @@
 const Travel = require("../models/viewAllTravel");
+
+// module.exports.index = async (req, res) => {
+//     const travels = await Travel.find({});
+//     res.render("travel/index", { travels });
+//     // or res.render('travels/index', {travels})
+// };
+
 module.exports.index = async (req, res) => {
-    const travels = await Travel.find({});
-    res.render("travel/index", { travels });
-    // or res.render('travels/index', {travels})
+    const viewAllTravel = await Travel.find({});
+    res.render("travel/index", { viewAllTravel });
 };
+
+// module.exports.renderNewForm = (req, res) => {
+//     res.render("/travel/new");
+// };
 
 module.exports.renderNewForm = (req, res) => {
-    res.render("/travel/new");
+    res.render("travel/new");
 };
 
-module.exports.createTravel = async (req, res, next) => {
-    const travel = new Travel(req.body.travel);
-    travel.author = req.user._id;
-    await Travel.save();
-    req.flash("success", "Made new travel!");
-    res.redirect(`/travel/${travel._id}`);
-};
+// module.exports.createTravel = async (req, res, next) => {
+//     const travel = new Travel(req.body.travel);
+//     travel.author = req.user._id;
+//     await Travel.save();
+//     req.flash("success", "Made new travel!");
+//     res.redirect(`/travel/${travel._id}`);
+// };
+
+module.exports.createTravel = (req, res, next) => {
+      const newTravel = new Travel(req.body.travel);
+      newTravel.author = req.user._id;
+      await newTravel.save();
+      req.flash("success", "Successfully listed a travel!");
+      res.redirect(`/travel/${newTravel._id}`);
+  };
+
+// module.exports.showTravel = async (req, res) => {
+//     const travel = await Travel.findById(req.params.id)
+//         .populate({
+//             path: "reviews",
+//             populate: { path: "author" },
+//         })
+//         .populate("author");
+//     console.log(travel);
+//     if (!travel) {
+//         req.flash("error", "Cant find that travel!");
+//         return res.redirect("/travel");
+//     }
+//     res.render("travel/edit", { travel });
+// };
+
 
 module.exports.showTravel = async (req, res) => {
-    const travel = await Travel.findById(req.params.id)
-        .populate({
-            path: "reviews",
-            populate: { path: "author" },
-        })
-        .populate("author");
-    console.log(travel);
-    if (!travel) {
-        req.flash("error", "Cant find that travel!");
-        return res.redirect("/travel");
-    }
-    res.render("travel/edit", { travel });
-};
+        const viewTravelId = await Travel.findById(req.params.id)
+            .populate({
+                path: "reviews",
+                populate: { path: "author"},
+            })
+            .populate("author");
+        console.log(viewTravelId);
+        if (!viewTravelId) {
+            req.flash("error", "No lo encuentra este viaje!");
+            return res.redirect("/travel");
+        }
+        res.render("travel/show", { viewTravelId });
+    };
+
+// module.exports.renderEditForm = async (req, res) => {
+//     const { id } = req.params;
+//     const travel = await Travel.findById(id);
+//     if (!travel) {
+//         req.flash("error", "Cant find that travel!");
+//         return res.redirect("/travel");
+//     }
+//     res.render("travel/edit", { travel });
+// };
 
 module.exports.renderEditForm = async (req, res) => {
-    const { id } = req.params;
-    const travel = await Travel.findById(id);
-    if (!travel) {
-        req.flash("error", "Cant find that travel!");
-        return res.redirect("/travel");
-    }
-    res.render("travel/edit", { travel });
-};
+      const { id } = req.params;
+      const editTravel = await Travel.findById(id);
+      if (!editTravel) {
+          req.flash("error", "Cannot be founded!");
+          return res.redirect("/travel");
+      }
+      res.render("travel/edit", { editTravel });
+  };
 
-module.exports.updateTravel = async (req, res) => {
-    const { id } = req.params;
-    const travel = await Travel.findByIdAndUpdate(id, { ...req.body.travel });
-    req.flash("success", "Updated travel!");
-    res.redirect(`/travel/${travel._id}`);
-};
+// module.exports.updateTravel = async (req, res) => {
+//     const { id } = req.params;
+//     const travel = await Travel.findByIdAndUpdate(id, { ...req.body.travel });
+//     req.flash("success", "Updated travel!");
+//     res.redirect(`/travel/${travel._id}`);
+// };
+
+module.exports.updateTravel = async (req, res, next) => {
+      const { id } = req.params;
+      const editedTravel = await Travel.findByIdAndUpdateid(id, {
+          ...req.body.travel,
+      });
+      req.flash("Updated a travel!");
+      res.redirect(`/travel/${editedTravel._id}`);
+  };
+
+
+// module.exports.deleteTravel = async (req, res) => {
+//     const { id } = req.params;
+//     await Travel.findByIdAndDelete(id);
+//     req.flash("success", "Deleted travel!");
+//     res.redirect("/travel");
+// };
 
 module.exports.deleteTravel = async (req, res) => {
-    const { id } = req.params;
-    await Travel.findByIdAndDelete(id);
-    req.flash("success", "Deleted travel!");
-    res.redirect("/travel");
-};
+      const { id } = req.params;
+      await Travel.findByIdAndDelete(id);
+      req.flash("Deleted a travel.");
+      res.redirect("/travel");
+  };
--- Spanish/routes/travel.js
+++ Spanish/routes/travel.js
@@ -11,6 +11,26 @@ const {
 
 // const ExpressError = require("../utils/ExpressError");
 const Travelall = require("../models/viewAllTravel");
+const travels = require("../controllers/travel");
+
+router.get("/", catchAsync(travels.index));
+router.get("/new", isLoggedIn, travels.renderNewForm);
+router.post("/", isLoggedIn, validateTravel, catchAsync(travels.createTravel));
+router.get("/:id", catchAsync(travels.showTravel));
+router.get(
+    "/:id/edit",
+    isLoggedIn,
+    isAuthor,
+    catchAsync(travels.renderEditForm)
+);
+router.put(
+    "/:id",
+    isLoggedIn,
+    isAuthor,
+    validateTravel,
+    catchAsync(travels.updateTravel)
+);
+router.delete("/:id", isLoggedIn, isAuthor, catchAsync(travels.deleteTravel));
 
 // ///TRAVEL
 // const validateTravel = (req, res, next) => {
@@ -34,19 +54,19 @@ const Travelall = require("../models/viewAllTravel");
 // };
 
 ///TRAVEL
-router.get(
-    "/",
-    // isLoggedIn,
-    catchAsync(async (req, res) => {
-        const viewAllTravel = await Travelall.find({});
-        res.render("travel/index", { viewAllTravel });
-    })
-);
+// router.get(
+//     "/",
+//     // isLoggedIn,
+//     catchAsync(async (req, res) => {
+//         const viewAllTravel = await Travelall.find({});
+//         res.render("travel/index", { viewAllTravel });
+//     })
+// );
 
 //TRAVEL
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("travel/new");
-});
+// router.get("/new", isLoggedIn, (req, res) => {
+//     res.render("travel/new");
+// });
 
 // router.get("/new", isLoggedIn, (req, res) => {
 //     if (!req.isAuthenticated()) {
@@ -57,60 +77,60 @@ router.get("/new", isLoggedIn, (req, res) => {
 // });
 
 ///TRAVEL
-router.post(
-    "/",
-    isLoggedIn,
-    validateTravel,
-    catchAsync(async (req, res, next) => {
-        const newTravel = new Travelall(req.body.travel);
-        newTravel.author = req.user._id;
-        await newTravel.save();
-        req.flash("success", "Successfully listed a travel!");
-        res.redirect(`/travel/${newTravel._id}`);
-    })
-);
+// router.post(
+//     "/",
+//     isLoggedIn,
+//     validateTravel,
+//     catchAsync(async (req, res, next) => {
+//         const newTravel = new Travelall(req.body.travel);
+//         newTravel.author = req.user._id;
+//         await newTravel.save();
+//         req.flash("success", "Successfully listed a travel!");
+//         res.redirect(`/travel/${newTravel._id}`);
+//     })
+// );
 
 ///TRAVEL
-router.get(
-    "/:id",
-    // isLoggedIn,
-    catchAsync(async (req, res) => {
-        const viewTravelId = await Travelall.findById(req.params.id)
-            .populate({
-                path: "reviews",
-                populate: {
-                    path: "author",
-                },
-            })
-            .populate("author");
-        console.log(viewTravelId);
-        if (!viewTravelId) {
-            req.flash("error", "No lo encuentra este viaje!");
-            return res.redirect("/travel");
-        }
-        res.render("travel/show", { viewTravelId });
-    })
-);
+// router.get(
+//     "/:id",
+//     // isLoggedIn,
+//     catchAsync(async (req, res) => {
+//         const viewTravelId = await Travelall.findById(req.params.id)
+//             .populate({
+//                 path: "reviews",
+//                 populate: {
+//                     path: "author",
+//                 },
+//             })
+//             .populate("author");
+//         console.log(viewTravelId);
+//         if (!viewTravelId) {
+//             req.flash("error", "No lo encuentra este viaje!");
+//             return res.redirect("/travel");
+//         }
+//         res.render("travel/show", { viewTravelId });
+//     })
+// );
 
 ///TRAVEL
-router.get(
-    "/:id/edit",
-    isLoggedIn,
-    isAuthor,
-    catchAsync(async (req, res) => {
-        const { id } = req.params;
-        const editTravel = await Travelall.findById(id);
-        if (!editTravel) {
-            req.flash("error", "Cannot be founded!");
-            return res.redirect("/travel");
-        }
-        // if (!editTravel.author.equals(req.user._id)) {
-        //     req.flash("error", "Cant touch this!");
-        //     return res.redirect(`/travel/${id}`);
-        // }
-        res.render("travel/edit", { editTravel });
-    })
-);
+// router.get(
+//     "/:id/edit",
+//     isLoggedIn,
+//     isAuthor,
+//     catchAsync(async (req, res) => {
+//         const { id } = req.params;
+//         const editTravel = await Travelall.findById(id);
+//         if (!editTravel) {
+//             req.flash("error", "Cannot be founded!");
+//             return res.redirect("/travel");
+//         }
+//         // if (!editTravel.author.equals(req.user._id)) {
+//         //     req.flash("error", "Cant touch this!");
+//         //     return res.redirect(`/travel/${id}`);
+//         // }
+//         res.render("travel/edit", { editTravel });
+//     })
+// );
 
 ///TRAVEL
 // router.put(
@@ -127,41 +147,41 @@ router.get(
 //     })
 // );
 
-router.put(
-    "/:id",
-    isLoggedIn,
-    isAuthor,
-    validateTravel,
-    catchAsync(async (req, res, next) => {
-        const { id } = req.params;
-        // const editedTravel = await Travelall.findById(id);
-        // if (!editedTravel.author.equals(req.user._id)) {
-        //     req.flash("error", "Cant touch this!");
-        //     return res.redirect(`/travel/${id}`);
-        //     //     ...req.body.travel,
-        // }
-        const editedTravel = await Travelall.findByIdAndUpdateid(id, {
-            ...req.body.travel,
-        });
-        req.flash("Updated a travel!");
-        res.redirect(`/travel/${editedTravel._id}`);
-    })
-);
+// router.put(
+//     "/:id",
+//     isLoggedIn,
+//     isAuthor,
+//     validateTravel,
+//     catchAsync(async (req, res, next) => {
+//         const { id } = req.params;
+//         // const editedTravel = await Travelall.findById(id);
+//         // if (!editedTravel.author.equals(req.user._id)) {
+//         //     req.flash("error", "Cant touch this!");
+//         //     return res.redirect(`/travel/${id}`);
+//         //     //     ...req.body.travel,
+//         // }
+//         const editedTravel = await Travelall.findByIdAndUpdateid(id, {
+//             ...req.body.travel,
+//         });
+//         req.flash("Updated a travel!");
+//         res.redirect(`/travel/${editedTravel._id}`);
+//     })
+// );
 
 // if (!editedTravel.author.equals(req.user._id)) {
 // }
 
 //TRAVEL
-router.delete(
-    "/:id",
-    isLoggedIn,
-    isAuthor,
-    catchAsync(async (req, res) => {
-        const { id } = req.params;
-        await Travelall.findByIdAndDelete(id);
-        req.flash("Deleted a travel.");
-        res.redirect("/travel");
-    })
-);
+// router.delete(
+//     "/:id",
+//     isLoggedIn,
+//     isAuthor,
+//     catchAsync(async (req, res) => {
+//         const { id } = req.params;
+//         await Travelall.findByIdAndDelete(id);
+//         req.flash("Deleted a travel.");
+//         res.redirect("/travel");
+//     })
+// );
 
 module.exports = router;
