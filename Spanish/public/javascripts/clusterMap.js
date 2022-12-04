d7f8aba1ebcb4afa66052a41f6cb7c06e1dac28e
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-12-04 16:11:03 +0300

    Added map navigation control on clusterMap.js

--- Spanish/public/javascripts/clusterMap.js
+++ Spanish/public/javascripts/clusterMap.js
@@ -8,6 +8,9 @@ const map = new mapboxgl.Map({
 });
 console.log(viewAllTravels);
 
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, "top-right");
+
 map.on("load", () => {
     // Add a new source from our GeoJSON data and
     // set the 'cluster' option to true. GL-JS will
