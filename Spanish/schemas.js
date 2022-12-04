17485b85578bea50c9bf847984ed9a8d0fcdab00
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-12-04 16:52:35 +0300

    Added escapeHTML to some exports

--- Spanish/schemas.js
+++ Spanish/schemas.js
@@ -1,34 +1,6 @@
 const Joi = require("joi");
 const { builtinModules } = require("module");
 
-module.exports.spanishSchemaAlso = Joi.object({
-    travel: Joi.object({
-        // image: Joi.string().required(),
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        location: Joi.string().required(),
-    }).required(),
-    deleteImages: Joi.array(),
-});
-
-module.exports.spanishSchema = Joi.object({
-    card: Joi.object({
-        // card: Joi.string().required(),
-        // hint: Joi.string().required(),
-        english: Joi.string().required(),
-        spanish: Joi.string().required(),
-        hintOne: Joi.string().required(),
-        hintTwo: Joi.string().required(),
-    }).required(),
-});
-
-module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required(),
-        body: Joi.string().required(),
-    }).required(),
-});
-
 const extension = (joi) => ({
     type: "String",
     base: joi.String(),
@@ -48,4 +20,32 @@ const extension = (joi) => ({
             },
         },
     },
+});
+
+module.exports.spanishSchemaAlso = Joi.object({
+    travel: Joi.object({
+        // image: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML(),
+        location: Joi.string().required().escapeHTML(),
+    }).required(),
+    deleteImages: Joi.array(),
+});
+
+module.exports.spanishSchema = Joi.object({
+    card: Joi.object({
+        // card: Joi.string().required(),
+        // hint: Joi.string().required(),
+        english: Joi.string().required(),
+        spanish: Joi.string().required(),
+        hintOne: Joi.string().required(),
+        hintTwo: Joi.string().required(),
+    }).required(),
+});
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required(),
+        body: Joi.string().required().escapeHTML(),
+    }).required(),
 });
