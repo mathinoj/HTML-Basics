8e7c607f6a10bc853a37fa5a11b99d5e6e54bb98
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-12-10 15:15:40 +0300

    Added timer to first computer move

--- TTT/ttt.js
+++ TTT/ttt.js
@@ -1,14 +1,16 @@
 let clickOne = document.querySelector("#one");
 // console.log(numz);
 clickOne.addEventListener("click", function (e) {
+    // alert("1 Clicked");
+    let get1 = document.getElementById("one").innerHTML;
+    let replace1 = get1.replace(1, "X");
+    document.getElementById("one").innerHTML = replace1;
     if (one) {
-        // alert("1 Clicked");
-        let getOne = document.getElementById("one").innerHTML;
-        let replaceOne = getOne.replace(1, "X");
-        document.getElementById("one").innerHTML = replaceOne;
+        let get5 = document.getElementById("five").innerHTML;
+        let replace5 = get5.replace(5, "O");
+        const setTime = setTimeout(dropOhh, 2000);
+        function dropOhh() {
+            document.getElementById("five").innerHTML = replace5;
+        }
     }
 });
-
-// let str = document.getElementById("demo").innerHTML;
-// let res = str.replace(/blue/g, "red");
-// document.getElementById("demo").innerHTML = res;
