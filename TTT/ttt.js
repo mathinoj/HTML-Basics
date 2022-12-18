a774a27faa0e6b5902e3090ab0bb7fd4f797c317
Matthias Hinojosa <hinojosamatthias@gmail.com>
2022-12-18 11:52:38 +0300

    Added no re-click to box that is already clicked

--- TTT/ttt.js
+++ TTT/ttt.js
@@ -18,70 +18,42 @@ for (let i = 1; i <= inSpan.length; i++) {
 // console.log(rando);
 console.log();
 let clickBox = document.querySelector("#griddy");
-// console.log(clickBox.children);
-for (let i = 1; i <= clickBox.children.length; i++) {
-    // console.log(i);
-    // console.log(clickBox);
-    // console.log(clickBox.children);
-    // console.log(clickBox.children.length);
-    // const bart = Array.from(i);
-    // console.log(bart);
-}
 clickBox.addEventListener("click", function (e) {
-    // console.dir(e);
-    // console.dir(e.path);
-    // console.dir(e.path[0].id);
     let eyeD = e.path[0].id;
-    console.log(eyeD);
-    // console.dir(e.path[0].innerText);
-    const hitIt = e.path[0].innerText;
-    console.log("hitIt: " + hitIt);
-    // alert(`Square ${hitIt} was toca'd!`);
-    // let elementNum = document.getElementById(`hitIt`);
-    // console.log(elementNum);
-    // for (let i = 0; i <= elementNum.length; i++) {
-    //     console.log(i);
-    // }
+    //Grabs ID of element
 
+    const hitIt = e.path[0].innerText;
+    //This shows the innerText of an element
+
+    //--------------------------------------------------------------
     let count = document.getElementById("countClick").innerText;
-    // console.log(count);
     count++;
     let upCount = document.querySelector("#countClick");
     let showNumChange = (upCount.innerText = count);
     console.log(showNumChange);
+    //THIS is the counter used for the if/else statement for X/O ^^^^^^^
 
-    console.dir(e);
-    console.dir(e.path[0].innerText);
     let xoMan = e.path[0].innerText;
 
-    // console.log("hitIt: " + hitIt);
     let takeOneAway = hitIt - 1;
-    console.log(takeOneAway);
+    //This is for the INDEX. Subtracts actual number by 1.^^^^^^^^^^^^^^
 
-    // for (let i = 1; i <= matt.length; i++) {
-    // console.log("hitIt3: " + hitIt);
-
-    // console.log("this is i: " + i);
-    // console.log("Matt: " + matt);
-    // alert(i);
-    // console.log(matt.indexOf(i));
-
-    if (showNumChange % 2 != 0) {
-        // alert("X");
-        let getBoxX = document.getElementById(`${eyeD}`).innerHTML;
-        console.log(getBoxX);
+    /////////////////THESE grab the chosen boxes/////////////////
+    let getBoxX = document.getElementById(`${eyeD}`).innerHTML;
+    let getBoxO = document.getElementById(`${eyeD}`).innerHTML;
+    /////////////////THESE grab the chosen boxes/////////////////
+    if (showNumChange % 2 != 0 && getBoxX !== "O") {
         let replaceX = getBoxX.replace(`${hitIt}`, "X");
-        let matt = (document.getElementById(`${eyeD}`).innerHTML = replaceX);
+        let showX = (document.getElementById(`${eyeD}`).innerHTML = replaceX);
+        console.log(showX);
 
         // let getBoxX = document.getElementById(`${eyeD}`);
         // let createElemX = document.createElement("span");
         // createElemX.setAttribute("id", `${eyeD}`);
         // createElemX.innerText = "X";
         // getBoxX.append(createElemX);
-    } else {
-        // alert("O");
-        let getBoxO = document.getElementById(`${eyeD}`).innerHTML;
-        console.log(getBoxO);
+        // } else {
+    } else if (getBoxO !== "X") {
         let replaceO = getBoxO.replace(`${hitIt}`, "O");
         document.getElementById(`${eyeD}`).innerHTML = replaceO;
 
@@ -91,250 +63,64 @@ clickBox.addEventListener("click", function (e) {
         // createElemO.innerHTML = "O";
         // getBoxO.append(createElemO);
     }
-    const boxing = document.getElementById("griddy").innerText;
-    console.log(boxing);
-    const art = Array.from(boxing);
-    console.log(art);
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
-    const removeNz = art.filter((removal) => {
-        return removal !== "\n";
-        //https://bobbyhadz.com/blog/javascript-remove-empty-strings-from-array
-    });
-    console.log(removeNz);
-    const matthias = removeNz.map(Number);
-    console.log(matthias);
-    // console.log(matthias.length);
-    // console.log(matthias.indexOf(9));
-    // console.log(takeOneAway);
-    // const saver = matthias.splice(`${takeOneAway}`, 1, `${xoMan}`);
-    // console.log("saver: " + saver);
 
-    // console.log("current arrary w/matthias const: " + matthias);
-    // const maver = Array.from(saver);
-    // }
+    let one = document.getElementById("one").innerText;
+    let two = document.getElementById("two").innerText;
+    let three = document.getElementById("three").innerText;
+    let four = document.getElementById("four").innerText;
+    let five = document.getElementById("five").innerText;
+    let six = document.getElementById("six").innerText;
+    let seven = document.getElementById("seven").innerText;
+    let eight = document.getElementById("eight").innerText;
+    let nine = document.getElementById("nine").innerText;
+
+    // console.dir(e);
+    let going = e.path[0].innerText;
+    console.log(going);
     // console.dir(e.path[0].innerText);
-    console.log("w/matthias const: " + matthias);
-    // for (let i = 0; i <= matthias.length; i++) {
-    console.log(matthias);
-    console.log("removeNz: " + removeNz);
-    console.log(Object.values(removeNz));
-    let rst = Object.values(removeNz).length;
-    // console.log(rst);
-    //     const firstValue = Object.values(obj)[0]; // 👉️ "Chile"
-    // console.log(firstValue);
 
-    // const firstValue = Object.values(removeNz)[8]; // 👉️ "Chile"
-    // console.log(firstValue);
-
-    const tart = Array.from(matthias);
-    // console.log(
-    //     `You clicked ${hitIt}, the index for this is: ` + tart.indexOf(saver)
-    // );
-    // console.log(
-    //     `You clicked ${hitIt}, the index for this [0] is: ` + tart.indexOf("0")
-    // );
-    // console.log(tart.length);
-    // console.log(matthias.length);
-
-    for (let i = 0; i < removeNz.length; i++) {
-        // console.log(i);
-        let firstValue = Object.values(removeNz)[i]; // 👉️ "Chile"
-        console.log(firstValue);
-
-        let one = document.getElementById("one").innerText;
-        console.log(one);
-        let two = document.getElementById("two").innerText;
-        let three = document.getElementById("three").innerText;
-        let four = document.getElementById("four").innerText;
-        let five = document.getElementById("five").innerText;
-        let six = document.getElementById("six").innerText;
-        let seven = document.getElementById("seven").innerText;
-        let eight = document.getElementById("eight").innerText;
-        let nine = document.getElementById("nine").innerText;
-
-        console.dir(e);
-        console.dir(e.path);
-        console.dir(e.path[0]);
-        // console.dir(e.path[0].innerText);
-        let going = e.path[0].innerText;
-        console.log(going);
-        // console.dir(e.path[0].innerText);
-
-        // console.log(removeNz);
-
-        // console.log(firstValue.indexOf(8));
-        // console.log(matthias.indexOf(i));
-        // let minusOne = matthias.length - 1;
-        // console.log(minusOne);
-        // const bee = Array.from(minusOne);
-        // console.log(bee);
-        // let indexed = matthias.indexOf(i);
-        // console.log(indexed);
-        // let plusX = "1" + i;
-        // console.log(plusX);
-        // let plusO = "0" + i;
-        // console.log(plusO);
-        ////////////////////////////////////////////////////////////////////////
-        // let newArr = saver.concat(matthias);
-        // console.log(newArr);
-        // const tart = Array.from(matthias);
-        // console.log("saver: " + saver);
-        // let `hit${hitIt}` =
-        // console.log("w/ indexOf(i): " + matthias.indexOf(i));
-        // if (going === "X") {
-        if (
-            (one === "X" && five === "X" && nine === "X") ||
-            (three === "X" && five === "X" && seven === "X") ||
-            (one === "X" && four === "X" && seven === "X") ||
-            (two === "X" && five === "X" && eight === "X") ||
-            (three === "X" && six === "X" && nine === "X") ||
-            (one === "X" && two === "X" && three === "X") ||
-            (four === "X" && five === "X" && six === "X") ||
-            (seven === "X" && eight === "X" && nine === "X")
-        ) {
-            alert("win");
-            break;
-        } else if (
-            (one === "O" && five === "O" && nine === "O") ||
-            (three === "O" && five === "O" && seven === "O") ||
-            (one === "O" && four === "O" && seven === "O") ||
-            (two === "O" && five === "O" && eight === "O") ||
-            (three === "O" && six === "O" && nine === "O") ||
-            (one === "O" && two === "O" && three === "O") ||
-            (four === "O" && five === "O" && six === "O") ||
-            (seven === "O" && eight === "O" && nine === "O")
-        ) {
-            alert("O win!");
-            break;
-        }
+    if (
+        (one === "X" && five === "X" && nine === "X") ||
+        (three === "X" && five === "X" && seven === "X") ||
+        (one === "X" && four === "X" && seven === "X") ||
+        (two === "X" && five === "X" && eight === "X") ||
+        (three === "X" && six === "X" && nine === "X") ||
+        (one === "X" && two === "X" && three === "X") ||
+        (four === "X" && five === "X" && six === "X") ||
+        (seven === "X" && eight === "X" && nine === "X")
+    ) {
+        alert("X wins!");
+        // break;
+    } else if (
+        (one === "O" && five === "O" && nine === "O") ||
+        (three === "O" && five === "O" && seven === "O") ||
+        (one === "O" && four === "O" && seven === "O") ||
+        (two === "O" && five === "O" && eight === "O") ||
+        (three === "O" && six === "O" && nine === "O") ||
+        (one === "O" && two === "O" && three === "O") ||
+        (four === "O" && five === "O" && six === "O") ||
+        (seven === "O" && eight === "O" && nine === "O")
+    ) {
+        alert("O wins!");
+        // break;
     }
-    // console.log("all array update w/ removeNz: " + removeNz);
-    let best = Object.values(removeNz);
-    console.log("Array should update: " + best);
 
-    // console.log("all array update w/ firstValue: " + firstValue);
-});
+    //THIS WAS AN ATTEMPT TO CHANGE THE ARRAY!!!!!!!!!!!!!
+    // const boxing = document.getElementById("griddy").innerText;
+    // const art = Array.from(boxing);
+    // // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+    // const removeNz = art.filter((removal) => {
+    //     return removal !== "\n";
+    //     //https://bobbyhadz.com/blog/javascript-remove-empty-strings-from-array
+    // });
+    // const matthias = removeNz.map(Number);
 
-let clickBoxWin = document.querySelector("#griddy");
-clickBoxWin.addEventListener("click", function (e) {
-    //     console.dir(e.path[0]);
-    //     let eyeDa = e.path[0].id;
-    //     let getBoxXz = document.getElementById(`${eyeDa}`).innerHTML;
-    //     console.log(eyeDa);
-    //     console.log(getBoxXz.indexOf());
-
-    //     console.log(clickBoxWin);
-    //     console.log(clickBoxWin.children);
-
-    //     let boxing = document.getElementById("griddy").innerText;
-    //     const art = Array.from(boxing);
-    //     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
-    //     const removeNz = art.filter((removal) => {
-    //         return removal !== "\n";
-    //         //https://bobbyhadz.com/blog/javascript-remove-empty-strings-from-array
-    //     });
-    //     let matthias = removeNz.map(Number);
-    //     console.dir(e);
-    //     console.dir(e.path[0].innerText);
-
-    //     // console.log("hitItHere: " + hitIt);
-
-    //     let newArray = e.path[0].innerText;
-    //     console.log(Array.from(newArray));
-
-    //     // let set = `"${bet}"`;
-    //     // console.log(set);
-    //     console.log(matthias);
-    //     // console.log(matthias.replace("NaN", `${newArray}`));
-
-    //     console.dir(e);
-    //     // let nice = matthias.splice(0, `"${set}"`);
-    //     // https://www.fwait.com/how-to-include-double-quotes-in-a-string-in-javascript/
-    //     // console.log(nice);
-    //     let counter = document.getElementById("countClick").innerText;
-    //     // console.log(count);
-    //     // counter++;
-    //     let upCount2 = document.querySelector("#countClick");
-    //     let showNumChanged = (upCount2.innerText = counter);
-    //     // console.log(showNumChanged);
-
-    //     // console.log(matthias.indexOf(e.path[0]));
-
-    //     // for (let i = 1; i <= matthias.length; i++) {
-    //     // console.log(i);
-    //     // console.log(matthias);
-    //     // console.log(matthias.indexOf(i));
-
-    //     // let indexed = matthias.indexOf(i);
-    //     // console.log(indexed);
-
-    //     // console.log(matthias);
-    //     // if ((indexed = "NaN" && showNumChanged % 2 != 0)) {
-    //     //     console.log(indexed);
-    //     //     console.log(matthias);
-    //     // }
-
-    //     // console.log(showNumChanged);
-
-    //     // console.log(matthias.indexOf(9));
-    //     // console.dir(e.path[0]);
-
-    //     // const x = matthias.indexOf(matthias[i], i + 1);
-    //     // console.log(`${i}: ${x}`);
-    //     // https://stackoverflow.com/questions/59396013/javascript-using-an-indexof-inside-for-loop
-    //     // console.log(clickBoxWin.children[indexed]);
-    //     // console.log(clickBoxWin.children[indexed].innerText);
-    //     // }
-
-    //     console.log(clickBoxWin.children[0]);
-    //     console.log(clickBoxWin.children[0].innerText);
-
-    //     // console.log(clickBoxWin);
-    //     // console.dir(e);
-    //     // console.dir(e.path);
-    //     // console.dir(e.path[0]);
-    //     // const hitIt = e.path[0].innerText;
-    //     // console.log("hitIt: " + hitIt);
-    //     // if (hitIt == 1 && hitIt == 5 && hitIt == 9) {
-    //     //     alert("win");
-    //     // }
-    //     let eyeD = e.path[0].id;
-    //     console.log(eyeD);
-    //     let eyeDz = e.path[0].innerText;
-    //     console.log(eyeDz);
-    //     console.log(matthias);
-    //     let splicer = matthias.splice(4, 1);
-    //     // console.log(splicer);
-    //     console.log(matthias);
-
-    //     let getBoxX = document.getElementById(`${eyeD}`).innerHTML;
-    //     console.log(getBoxX);
-
-    //     // if (getBoxX === "X") {
-    //     //     alert("win");
-    //     // }
-    const hitIt = e.path[0].innerText;
-    // console.log(hitIt);
-    let xoMan = e.path[0].innerText;
-
-    let takeOneAway = hitIt - 1;
-    // console.log(takeOneAway);
-
-    const boxing = document.getElementById("griddy").innerText;
-    const art = Array.from(boxing);
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
-    const removeNz = art.filter((removal) => {
-        return removal !== "\n";
-        //https://bobbyhadz.com/blog/javascript-remove-empty-strings-from-array
-    });
-    const matthias = removeNz.map(Number);
-    // console.log(matthias);
-    // console.log(takeOneAway);
-    const saver = matthias.splice(`${takeOneAway}`, 1, `${xoMan}`);
-    // console.log("current arrary w/matthias const: " + matthias);
-    // const maver = Array.from(saver);
-    // }
-    // console.dir(e.path[0].innerText);
-    // console.log("w/matthias const: " + matthias);
+    // let rst = Object.values(removeNz).length;
     // const tart = Array.from(matthias);
+
+    // for (let i = 0; i < removeNz.length; i++) {
+    //     let firstValue = Object.values(removeNz)[i]; // 👉️ "Chile"
+    //     console.log(firstValue);
+    // }
+    //THIS WAS AN ATTEMPT TO CHANGE THE ARRAY!!!!!!!!!!!!!
 });
