d2811ef25045fdf1b6e4ac4e5b0fe78b38dfde24
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-01-22 17:34:54 +0300

    added session.returnTo to middleware.js

--- Idioma/middleware.js
+++ Idioma/middleware.js
@@ -3,6 +3,7 @@ module.exports.isLoggedIn = (req, res, next) => {
     //we export this middleware by doing module.exports.isLoggedIn, but then we have to import it on the cards.js and which ever other router/router-file we want to use this middleware.
     if (!req.isAuthenticated()) {
         //'isAuthenticated()', has to deal with how information is stored/retrieved from the session. This method is automatically added to the REQUEST object itself.
+        req.session.returnTo = req.originalUrl;
         req.flash("error", "Must be signed in!");
         return res.redirect("/login");
     }
--- Idioma/routes/users.js
+++ Idioma/routes/users.js
@@ -16,7 +16,6 @@ router.post(
             const user = new User({ email, username });
             const registeredUser = await User.register(user, password);
             req.login(registeredUser, (err) => {
-                //we call REQ.LOGIN after we have reg'd a user, and we pass in the registeredUser that we created above.
                 if (err) return next(err);
                 req.flash("success", "You have successfully reg'd");
                 res.redirect("/cards");
@@ -27,11 +26,6 @@ router.post(
         }
     })
 );
-//when we create a new user the user is not kept logged in and instead they have to go back to the login page to log back in, which sucks its not conveneint.
-//We want to fix this but we don't want to just flash a successful login and then redirect them to the login page
-//instead we want to login them in upon creation of a user
-//the way we do this is with the PASSPORT helper method called 'LOGIN', which establishes a login session.
-//REQ.LOGIN is primarily used when users signup, during which REQ.LOGIN() can be invoked to autmotaically login the newly reg'd user.
 
 router.get("/login", (req, res) => {
     res.render("users/login");
