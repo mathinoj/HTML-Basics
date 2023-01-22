7d20df12d45bf76f27720f5e49ad328f50dacf6a
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-01-22 16:50:31 +0300

    added next to router.post

--- Idioma/routes/users.js
+++ Idioma/routes/users.js
@@ -10,12 +10,13 @@ router.get("/register", (req, res) => {
 
 router.post(
     "/register",
-    catchAsync(async (req, res) => {
+    catchAsync(async (req, res, next) => {
         try {
             const { email, username, password } = req.body;
             const user = new User({ email, username });
             const registeredUser = await User.register(user, password);
             req.login(registeredUser, (err) => {
+                //we call REQ.LOGIN after we have reg'd a user, and we pass in the registeredUser that we created above.
                 if (err) return next(err);
                 req.flash("success", "You have successfully reg'd");
                 res.redirect("/cards");
@@ -26,6 +27,11 @@ router.post(
         }
     })
 );
+//when we create a new user the user is not kept logged in and instead they have to go back to the login page to log back in, which sucks its not conveneint.
+//We want to fix this but we don't want to just flash a successful login and then redirect them to the login page
+//instead we want to login them in upon creation of a user
+//the way we do this is with the PASSPORT helper method called 'LOGIN', which establishes a login session.
+//REQ.LOGIN is primarily used when users signup, during which REQ.LOGIN() can be invoked to autmotaically login the newly reg'd user.
 
 router.get("/login", (req, res) => {
     res.render("users/login");
