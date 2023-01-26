bd02e5f8a3fcdddf0edef9b29d642f543f0d503a
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-01-26 10:58:32 +0300

    Added seedDbIdioma with current author

---
+++ Idioma/seeds/seeds.js
@@ -0,0 +1,96 @@
+const mongoose = require("mongoose");
+const Idioma = require("../models/idioma");
+
+mongoose.connect("mongodb://localhost:27017/idioma", {});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("DB connected!");
+});
+
+// const c = new Idioma({ spanish: "hola" });
+// await c.save(); THIS WAS A TEST DONE IN MOD 412
+const seedDbIdioma = async () => {
+    await Idioma.deleteMany({});
+    const cardy = new Idioma({
+        author: "63c94a747495e6f4063670aa",
+    });
+    await cardy.save();
+};
+
+//set the loop to run 50 times to get a city
+
+seedDbIdioma().then(() => {
+    mongoose.connection.close();
+});
+
+//this file you run on its own anytime you want to get some new data in DB, which is pretty common - to seed your DB sepertately from a web app for development purposes. We want some data in there and we want to isolate it from the actual index.js/app.js of the application youre building
+
+const seedLanguages = [
+    {
+        english: "Hello",
+        spanish: "Hola",
+        hintOne: "Starts with H",
+        hintTwo: "Ends with A",
+        number: 8,
+    },
+    {
+        english: "Yes",
+        spanish: "Si",
+        hintOne: "Starts with S",
+        hintTwo: "Ends with I",
+        number: 7,
+    },
+    {
+        english: "Bye",
+        spanish: "Adios",
+        hintOne: "Starts with B",
+        hintTwo: "Ends with S",
+        number: 6,
+    },
+    {
+        english: "One",
+        spanish: "Uno",
+        hintOne: "Starts with U",
+        hintTwo: "Ends with O",
+        number: 5,
+    },
+    {
+        english: "Monday",
+        spanish: "Lunes",
+        hintOne: "Starts with L",
+        hintTwo: "Ends with S",
+        number: 4,
+    },
+    {
+        english: "Where",
+        spanish: "Donde",
+        hintOne: "Starts with D",
+        hintTwo: "Ends with E",
+        number: 3,
+    },
+    {
+        english: "Head",
+        spanish: "Cabeza",
+        hintOne: "Starts with C",
+        hintTwo: "Ends with A",
+        number: 2,
+    },
+    {
+        english: "Five",
+        spanish: "Cinco",
+        hintOne: "Starts with C",
+        hintTwo: "Ends with O",
+        number: 1,
+    },
+];
+
+Idioma.insertMany(seedLanguages)
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+//One thing to know about insertManyy in Mongoose is that if anything does not pass validation, then nothing will be inserted at least by default. So Mongoose validates all of this before it inserts anything, and then it inserts it in one go. So just know that if you do have validations and we're failing, everything will fail to insert.
