cb49ad26f29c305bffafd1e4f79da38a9eeda254
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-04-21 11:35:38 -0600

    Removed previous attempts, and left note

--- Idioma/routes/users.js
+++ Idioma/routes/users.js
@@ -10,46 +10,17 @@ router.get("/register", (req, res) => {
     res.render("users/register");
 });
 
-// const addIt = db.collection("addedcards");
-// let adder = { addedCard: newCard };
-// let result = addIt.insertOne(adder);
-// console.log("result: " + result);
-
 router.post(
     "/register",
     catchAsync(async (req, res, next) => {
         try {
             const { email, username, password } = req.body;
             const user = new User({ email, username });
-            // const li = new User({ username });
-            // console.log("li: " + li);
-            // console.log("USERNAME: " + username);
+            //The next 3 lines add a new user to addedCard.js Schema *********
             let x = new AddedCard();
-            // x.nowUser = username;
-            // x.nowUser.push(user); THISS WORKS BUT TREATS LIKE AN ARRAY!!
-            // console.log("x: " + x);
             x.nowUser = user;
-            const registeredUser = await User.register(user, password);
             await x.save();
-            console.log("again x: " + x);
-
-            // const newCard = new Idioma(req.body.newCard);
-            // newCard.author = req.user._id;
-            // await newCard.save();
-
-            // console.log("regd: " + registeredUser);
-
-            // const newCard = new Idioma(req.body.newCard);
-            // newCard.author = req.user._id;
-            // await newCard.save();
-            // console.log("newCard here: " + newCard);
-
-            // let findSelectedCard = await Idioma.findById(checkedBox);
-            //     const addedCardy = await User.findById(user);
-            //     findSelectedCard.addedCard.push(addedCardy);
-            //     await findSelectedCard.save();
-
-            // console.log("seeId: " + req.user._id);
+            const registeredUser = await User.register(user, password);
             req.login(registeredUser, (err) => {
                 if (err) return next(err);
                 req.flash("success", "You have successfully reg'd");
