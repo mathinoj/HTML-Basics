efb0bbd0f7736d2e1babf67be741d5f06acaa225
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-03 14:51:50 -0600

    Created Friend folder for new prjct

--- Idioma/middleware.js
+++ Idioma/middleware.js
@@ -3,6 +3,8 @@ const ExpressError = require("./utils/ExpressError");
 const Idioma = require("./models/idioma");
 const User = require("./models/user");
 const addedCards = require("./models/addedCards.js");
+const { func } = require("joi");
+const { db } = require("./models/idioma");
 
 module.exports.isLoggedIn = (req, res, next) => {
     // console.log("REQ.USER: ", req.user); TEST MOD 517
@@ -54,6 +56,8 @@ module.exports.paginate = async (req, res, next) => {
     let page = parseInt(req.params.page) || 1;
     let selection = req.query.selections;
 
+    // console.log("ParSint: " + parseInt(req.params.page));
+
     Idioma.find({})
         .skip(perPage * page - perPage) //THIS I BELIEVE SETS initial pg to 0, W/O i believe we don't get the the nxt btn to goto nxt pg.
         .limit(perPage)
@@ -68,7 +72,11 @@ module.exports.paginate = async (req, res, next) => {
                     req.flash("error", "Page cannot be found!");
                     return res.redirect("/cards");
                 }
-
+                console.log("perPage: " + perPage);
+                console.log("page: " + page);
+                console.log("count: " + count);
+                console.log("testy: " + matt);
+                console.log("testRound: " + roundedD);
                 // const checkD = addedCards.find({})
 
                 return res.render("cards/index", {
@@ -95,7 +103,6 @@ module.exports.paginateTest = async (req, res, next) => {
     // let entireUserInfo = res.locals.currentUser;
     // console.log("let: " + entireUserInfo);
     // let userIdNum = entireUserInfo.id;
-
     if (!req.user) {
         req.flash("error", "Must be logged in!");
         return res.redirect(`/cards`);
@@ -105,33 +112,100 @@ module.exports.paginateTest = async (req, res, next) => {
     let entireUserInfo = res.locals.currentUser;
     let userIdNum = entireUserInfo.id;
 
-    let randomDocsx = await User.findById(userIdNum).populate("addedCard");
+    let perPage = 1;
+    let page = 1;
+
+    const randomDocsx = await User.findById(userIdNum).populate("addedCard");
+
+    // await User.populate(users, { path: 'dog', select: 'name' });
+
     // let randomDocsx = await User.findById(userIdNum);
-    console.log("randomDocsx: " + randomDocsx.length);
 
-    // console.log("m: " + randomDocs.length);
-    // console.log("m addedC: " + m.addedCard);
-    // console.log("m addedC length: " + randomDocs.addedCard);
-    let randomDocs = randomDocsx.addedCard;
-    // let item = c[Math.floor(Math.random() * c.length)];
-    // console.log("item: " + item);
-    let testLength = randomDocs.length;
-    // let randomDocs = c
-    //     .sort(() => Math.random() - Math.random())
-    //     .slice(0, 5);
-    // https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array
-    console.log("shfld: " + testLength);
-
-    return res.render("cards/tested", {
-        // allCards: allCardsAgain,
-        // pages: Math.ceil(count / perPage),
-        // roundedD,
-        // count,
-        // page,
-        // err,
-        // current: page,
-        userIdNum,
-        randomDocs,
-    });
+    // console.log("randomDocsx: " + randomDocsx);
+    // console.log("randomDocsx added: " + randomDocsx.addedCard);
+    // console.log("randomDocsx lenth: " + randomDocsx.addedCard.length);
+    // let randomDocs = randomDocsx.addedCard;
+    // console.log("rL: " + randomDocs);
+    // for (let i = 1; i <= randomDocs.length; i++) {
+    //     console.log("see l: " + i);
+    //     console.log("rL: " + randomDocs);
     // }
+
+    User.findById(userIdNum)
+        // .populate("addedCard")
+        // .populate({ path: "addedCard", populate: { path: "addedCard" } })
+        .populate({ path: "addedCard", path: "addedCard" })
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec(function (err, randomDocs) {
+            if (err) return next(err.message);
+            User.findById(userIdNum)
+                .populate("addedCard")
+                .count()
+                .exec(function (err, count) {
+                    if (err) return next(err.message);
+                    const testy = count / perPage;
+                    const testRound = Math.ceil(testy);
+                    if (page > testRound) {
+                        req.flash("error", "Page not happening.");
+                        return res.redirect("/cards/myCards");
+                    }
+
+                    // console.log("Cadd: " + count.addedCard);
+                    console.log("perPage: " + perPage);
+                    console.log("page: " + page);
+                    console.log("count: " + count);
+                    console.log("testy: " + testy);
+                    console.log("testRound: " + testRound);
+
+                    // console.log("attTest Cz: " + allTestCards);
+
+                    // let seeHowMany = allTestCards.addedCard;
+                    // let seeLength = seeHowMany.length;
+                    // console.log("seeLength: " + seeLength);
+                    // for (let i = 1; i <= seeLength; i++) {
+                    //     // console.log("IIIII: " + i);
+                    //     // console.log("numz: " + seeHowMany);
+                    // }
+
+                    return res.render("cards/tested", {
+                        randomDockers: randomDocs,
+                        pages: Math.ceil(count / perPage),
+                        testRound,
+                        count,
+                        page,
+                        err,
+                        juh: count,
+                        // randomDocs,
+                    });
+                });
+        });
+
+    // let testLength = randomDocs.length;
+    // console.log("testLength: " + testLength);
+    // for (let i = 1; i < testLength; i++) {
+    //     console.log("IIII: " + i);
+    //     // let tPlusOne = i;
+
+    //     // let randomDocs = c
+    //     //     .sort(() => Math.random() - Math.random())
+    //     //     .slice(0, 5);
+    //     // https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array
+    //     // console.log("testL: " + testLength);
+    //     // console.log("tPLUS: " + tPlusOne);
+    //     // }
+    //     return res.render("cards/tested", {
+    //         // allCards: allCardsAgain,
+    //         // pages: Math.ceil(count / perPage),
+    //         // roundedD,
+    //         // count,
+    //         // page,
+    //         // err,
+    //         // current: page,
+    //         userIdNum,
+    //         randomDocs,
+    //         // i,
+    //         // tPlusOne,
+    //     });
 };
+// };
