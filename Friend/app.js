713272ee846ce5d36afceb3a745f4432157169d3
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-03 15:48:59 -0600

    Commented out res.send for test connection to home

--- Friend/app.js
+++ Friend/app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-    res.send("test if it wrx");
+    // res.send("test if it wrx");
     res.render("home");
 });
 
