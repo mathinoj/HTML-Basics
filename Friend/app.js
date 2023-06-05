603f6d797f13eea1154cc8d8240fe9b6119866ae
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-05 13:26:34 -0600

    Created layouts fldr, added boilerplate

--- Friend/app.js
+++ Friend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const Viewall = require("./models/viewAll");
 const methodOverride = require("method-override");
 
@@ -15,6 +16,7 @@ db.once("open", () => {
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
