cb9c7b7435530eb8f4ee836f1a68a1cffa6f894b
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-08 14:22:59 -0600

    sessionConfig added

--- Friend/app.js
+++ Friend/app.js
@@ -29,6 +29,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+const sessionConfig = {
+    secret: "examplesecretdontdoforreal",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
+app.use(session(sessionConfig));
 app.use("/cards", cards);
 
 app.get("/", (req, res) => {
