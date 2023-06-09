aa1baeeb959b305ce01988f8237aa13cd461cef5
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-09 15:47:07 -0600

    required isLoggedIn midware and const db

--- Friend/routes/cards.js
+++ Friend/routes/cards.js
@@ -4,6 +4,9 @@ const catchAsync = require("../utils/catchAsync");
 const { cardSchema } = require("../schema.js");
 const ExpressError = require("../utils/ExpressError");
 const Viewall = require("../models/viewAll");
+const { isLoggedIn } = require("../middleware");
+
+const db = mongoose.connection;
 
 const validateCard = (req, res, next) => {
     const { error } = cardSchema.validate(req.body);
