f18abda10ff88edf14ba7e835a4fed4b983924ad
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-10 13:06:37 -0600

    Moved seeds.js into seeds fldr

--- Friend/seeds/seeds.js
+++ Friend/seeds/seeds.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Viewall = require("./models/viewAll");
+const Viewall = require("../models/viewAll");
 
 mongoose.connect("mongodb://localhost:27017/friend", {});
 
