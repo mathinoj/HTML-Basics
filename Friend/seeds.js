41e5be64ac59a67e3c55c290e4517265574a90ae
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-10 13:05:40 -0600

    Added author to seeds

--- Friend/seeds.js
+++ Friend/seeds.js
@@ -14,36 +14,43 @@ const viewAllSeeds = [
         title: "Hello",
         price: 1,
         description: "First One",
+        author: "64839bce6fd85aa8d5335690",
     },
     {
         title: "Bye",
         price: 2,
         description: "Second One",
+        author: "64839bce6fd85aa8d5335690",
     },
     {
         title: "Hola",
         price: 3,
         description: "Third One",
+        author: "64839bce6fd85aa8d5335690",
     },
     {
         title: "Adios",
         price: 4,
         description: "Fourth One",
+        author: "64839bce6fd85aa8d5335690",
     },
     {
         title: "Hi",
         price: 5,
         description: "Fifth One",
+        author: "64839bce6fd85aa8d5335690",
     },
     {
         title: "Laters",
         price: 6,
         description: "Sixth One",
+        author: "64839bce6fd85aa8d5335690",
     },
     {
         title: "Yo",
         price: 7,
         description: "Seventh One",
+        author: "64839bce6fd85aa8d5335690",
     },
 ];
 
