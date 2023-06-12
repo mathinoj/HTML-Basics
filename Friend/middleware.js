b8afdeb2e4f6e190d5e302fd1d45a93333b558f3
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-12 14:21:44 -0600

    Required schema and expressError

--- Friend/middleware.js
+++ Friend/middleware.js
@@ -1,3 +1,6 @@
+const { cardSchema } = require("../schema.js");
+const ExpressError = require("../utils/ExpressError");
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
