27367d3ca8a69644fb4ae6c7506497b98ea432a2
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-13 12:29:35 -0600

    Displaying users that are currently logged in user

--- Friend/routes/friends.js
+++ Friend/routes/friends.js
@@ -11,19 +11,11 @@ const db = mongoose.connection;
 router.get(
     "/friend",
     catchAsync(async (req, res, next) => {
+        const currentUserId = req.user.id;
         const showUsers = await User.find({});
-        console.log("showUsers: " + showUsers);
-        console.log("friend: " + req.user.id);
-        res.render("friends/friend", { showUsers });
-
-        // router.get(
-        //     "/",
-        //     catchAsync(async (req, res, next) => {
-        //         const allCards = await Viewall.find({});
-        //         res.render("cards/index", { allCards });
-        //     })
-        // );
-        // res.render("cards/index", { allCards });
+        // console.log("showUsers: " + showUsers);
+        // console.log("friend: " + currentUserId);
+        res.render("friends/friend", { showUsers, currentUserId });
     })
 );
 
