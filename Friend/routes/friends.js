ab2894201751479b8ddaa8f9eb955427524ce082
Matthias Hinojosa <hinojosamatthias@gmail.com>
2023-06-14 11:39:40 -0600

    fixed the link to yourFriends and did get router

--- Friend/routes/friends.js
+++ Friend/routes/friends.js
@@ -15,14 +15,11 @@ router.get(
         res.locals.currentUser = req.user;
         let tryIt = res.locals.currentUser;
         let currentUserId = tryIt.id;
-        // console.log("currentUserId: " + currentUserId);
         const showUsers = await User.find({});
-        // console.log("showUsers: " + showUsers);
         const showFriend = await Friend.findById(currentUserId).populate(
             "requests"
         );
         console.log("showFrnd: " + showFriend);
-        // console.log("friend: " + currentUserId);
         res.render("friends/friend", { showUsers, currentUserId });
     })
 );
@@ -34,34 +31,22 @@ router.get(
         let userFriending = res.locals.currentUser;
         const requestFriend = await User.findById(req.params.id);
         if (requestFriend) {
-            // let findSelectedCard = await Idioma.findById(checkedBox);
-            // const addedCardy = await User.findById(user);
-            // findSelectedCard.addedCard.push(addedCardy);
-            // await findSelectedCard.save();
-            // console.log("reqFrendID: " + requestFriend.id);
-            //
-            // let findSelectedCardAgain = await Idioma.findById(getTestedCard);
-            // let findUser = await User.findById(req.user._id);
-            // findUser.addedCard.push(findSelectedCardAgain);
-            // await findUser.save();
-            console.log("reqFREND: " + requestFriend);
-
-            let iWantFriendId = requestFriend.id;
-            console.log("userFriending: " + userFriending.id);
-            // const friendingId = userFriending;
-            // let findFriending = await Friend.findById(iWantFriendId);
+            // let iWantFriendId = requestFriend.id;
+            // console.log("userFriending: " + userFriending.id);
             let findFriending = await Friend.findById(userFriending.id);
-            console.log("findFriending: " + findFriending);
-            let blu = findFriending.requests.push(requestFriend);
-            console.log("blu: " + blu);
+            // console.log("findFriending: " + findFriending);
+            findFriending.requests.push(requestFriend);
             await findFriending.save();
-            // console.log("userFriendingID: " + userFriending.id);
-            // console.log("userFriendingNAME: " + userFriending.username);
-            console.log("reqFrnd: " + requestFriend);
-
             req.flash("success", "Sent request!");
             return res.redirect("/friend");
         }
+        // res.render("friends/yourFriends");
+    })
+);
+
+router.get(
+    "/yourFriends",
+    catchAsync(async (req, res, next) => {
         res.render("friends/yourFriends");
     })
 );
